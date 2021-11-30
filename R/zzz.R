36089120a97db899cd78c8bfc6ca2ef57860f9a3
Lukas Burk <burk@leibniz-bips.de>
2021-11-30 16:07:07 +0100

    Add learners to mlr_learners dict

---
+++ R/zzz.R
@@ -0,0 +1,15 @@
+register_mlr3 = function() {
+  x = utils::getFromNamespace("mlr_learners", ns = "mlr3")
+
+  # classification learners
+  x$add("classif.torch.tabnet", LearnerClassifTorchTabnet)
+
+
+  # regression learners
+  x$add("regr.torch.tabnet", LearnerRegrTorchTabnet)
+
+}
+
+.onLoad = function(libname, pkgname) { # nolint
+  register_namespace_callback(pkgname, "mlr3", register_mlr3)
+}
