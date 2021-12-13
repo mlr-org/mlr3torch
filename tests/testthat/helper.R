2fff47d6613944ada1c8e8aca35dbd30d7e506d4
Lukas Burk <burk@leibniz-bips.de>
2021-12-13 14:03:00 +0100

    Load mlr3misc for tests b/c str_collapse was not found

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,5 +1,6 @@
 library(checkmate)
 library(mlr3)
+library(mlr3misc)
 # Load test scaffolding without helper_debugging.R temporarily
 # See https://github.com/mlr-org/mlr3torch/issues/8
 lapply(list.files(system.file("testthat", package = "mlr3"), pattern = "^helper.*\\.[rR]$", full.names = TRUE)[-2], source)
