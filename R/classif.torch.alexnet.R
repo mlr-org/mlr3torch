127ff63144e6adaed500ae2648bbd67f2e2c5dae
Lukas Burk <burk@leibniz-bips.de>
2022-01-31 18:02:24 +0100

    Experiment with alexnet learner

---
+++ R/classif.torch.alexnet.R
@@ -0,0 +1,258 @@
+#' @title Classification AlexNet Learner
+#' @author Lukas Burk
+#' @name mlr_learners_classif.torch.alexnet
+#'
+#' @template class_learner
+#' @templateVar id classif.torch.alexnet
+#' @templateVar caller model_alexnet
+#' @references
+#'
+#' @template seealso_learner
+#' @export
+#' @examples
+#' \dontrun{
+#' library(mlr3)
+#' library(mlr3torch)
+#' }
+LearnerClassifTorchAlexNet = R6::R6Class("LearnerClassifTorchAlexNet",
+  inherit = LearnerClassif,
+
+  public = list(
+    #' @description
+    #' Creates a new instance of this [R6][R6::R6Class] class.
+    initialize = function() {
+      ps <- ParamSet$new(list(
+        ParamLgl$new("pretrained",  default = TRUE, tags = "train"),
+        ParamInt$new("num_threads", default = 1L, lower = 1L, upper = Inf, tags =  c("train", "control")),
+        ParamInt$new("batch_size",  default = 256L, lower = 1L, upper = Inf, tags = "train"),
+        ParamFct$new("loss",        default = "cross_entropy", levels = c("cross_entropy"), tags = "train"),
+        ParamInt$new("epochs",      default = 5L,  lower = 1L, upper = Inf, tags = "train"),
+        ParamLgl$new("drop_last",   default = TRUE, tags = "train"),
+        ParamDbl$new("valid_split", default = 0.2, lower = 0, upper = 1, tags = "train"),
+        #ParamDbl$new("learn_rate",  default = 0.02, lower = 0, upper = 1, tags = "train"),
+        ParamInt$new("step_size", default = 1, lower = 1, upper = Inf, tags = "train"),
+        # FIXME: Shoddy placeholder, needs more thinking
+        ParamUty$new("img_transforms",  default = NULL, tags = "train"),
+        # FIXME: Currently either 'adam' or arbitrary optimizer function according to docs
+        ParamUty$new("optimizer",   default = "adam", tags = "train"),
+        ParamLgl$new("verbose",     default = TRUE, tags = "control"),
+        ParamFct$new("device",      default = "auto", levels = c("auto", "cpu", "cuda"), tags = "control")
+      ))
+
+      # Set param values that differ from default in tabnet_fit
+      ps$values = list(
+        pretrained = TRUE,
+        num_threads = 1L,
+        batch_size = 128L,
+        loss = "cross_entropy",
+        epochs = 10L,
+        drop_last = TRUE,
+        valid_split = 0.2,
+        step_size = 1,
+        # FIXME: Figure out transform placement
+        img_transforms = NULL,
+        optimizer = "adam",
+        verbose = TRUE,
+        device = "auto"
+      )
+
+      super$initialize(
+        id = "classif.torch.alexnet",
+        packages = c("torchvision", "torch"),
+        feature_types = c("imageuri"),
+        predict_types = c("response", "prob"),
+        param_set = ps,
+        properties = c("multiclass", "twoclass"),
+        man = "mlr3torch::mlr_learners_classif.torch.alexnet"
+      )
+    }
+  ),
+
+  private = list(
+
+    .train = function(task) {
+      # get parameters for training
+      pars = self$param_set$get_values(tags = "train")
+      pars_control = self$param_set$get_values(tags = "control")
+
+      # Drop control par from training pars
+      pars <- pars[!(names(pars) %in% names(pars_control))]
+
+      # Set number of threads
+      torch::torch_set_num_threads(pars_control$num_threads)
+
+      # set column names to ensure consistency in fit and predict
+      # (pasted from tabnet learner, unlikely to be useful?)
+      # self$state$feature_names = task$feature_names
+
+      # Validation split & datasets/loaders-------------------------------------
+      val_idx <- sample(task$nrow, floor(task$nrow * pars$valid_split))
+      train_idx <- setdiff(seq_len(task$nrow), val_idx)
+
+      train_ds <- img_dataset(task$data(), row_ids = train_idx, transform = pars$img_transforms)
+      valid_ds <- img_dataset(task$data(), row_ids = val_idx, transform = pars$img_transforms)
+
+      train_dl <- torch::dataloader(train_ds, batch_size = pars$batch_size, shuffle = TRUE, drop_last = pars$drop_last)
+      valid_dl <- torch::dataloader(valid_ds, batch_size = pars$batch_size, shuffle = FALSE, drop_last = pars$drop_last)
+
+      # browser()
+      train_alexnet(
+        pretrained = pars$pretrained,
+        train_dl = train_dl, valid_dl = valid_dl,
+        num_classes = length(img_task$class_names),
+        optimizer = pars$optimizer,
+        #scheduler,
+        step_size = pars$step_size,
+        loss = pars$loss,
+        epochs = pars$epochs,
+        verbose = pars_control$verbose,
+        device = pars_control$device
+      )
+
+    },
+
+    .predict = function(task) {
+      # get parameters with tag "predict"
+      pars = self$param_set$get_values(tags = "predict")
+      pars_control = self$param_set$get_values(tags = "control")
+
+      # Drop control par from training pars
+      pars <- pars[!(names(pars) %in% names(pars_control))]
+
+      # get newdata and ensure same ordering in train and predict
+      # newdata = task$data(cols = self$state$feature_names)
+
+      # if (self$predict_type == "response") {
+      #   pred = mlr3misc::invoke(predict, self$model, new_data = newdata,
+      #                           type = "class", .args = pars)
+      #
+      #   list(response = pred[[".pred_class"]])
+      # } else {
+      #   pred = mlr3misc::invoke(predict, self$model, new_data = newdata,
+      #                           type = "prob", .args = pars)
+      #
+      #   # Result will be a df with one column per variable with names '.pred_<level>'
+      #   # we want the names without ".pred"
+      #   names(pred) <- sub(pattern = ".pred_", replacement = "", names(pred))
+      #
+      #   list(prob = as.matrix(pred))
+      # }
+
+      # FIXME: hardcoded transform for prediction
+      img_transforms <- function(img) {
+        img %>%
+          # first convert image to tensor
+          torchvision::transform_to_tensor() %>%
+          # # then move to the GPU (if available)
+          (function(x) x$to(device = pars_control$device)) %>%
+          # Required resize for alexnet
+          torchvision::transform_resize(c(64,64))
+      }
+
+      # FIXME: Ad hoc dataloader from input task with 1 possibly huge batch
+      test_ds <- img_dataset(task$data(), transform = img_transforms)
+      test_dl <- torch::dataloader(test_ds, batch_size = 1, shuffle = TRUE, drop_last = FALSE)
+
+      self$model$eval()
+
+      # FIXME: Collect class predictions / class probs
+      torch::with_no_grad({
+        coro::loop(for (b in test_dl) {
+          pred <- self$model(batch[[1]]$to(device = pars_control$device))
+          #
+          pred <- pred$argmax()
+        })
+      })
+
+    }
+  )
+)
+
+#' Wrapper to fit alexnet within learner
+#' @keywords internal
+#' @return Trained nn_module object
+train_alexnet <- function(
+  pretrained = FALSE,
+  train_dl, valid_dl,
+  num_classes,
+  optimizer = "adam",
+  #scheduler,
+  step_size = 1,
+  loss,
+  epochs,
+  verbose,
+  device
+  ) {
+
+  if (device == "auto") device <- if (torch::cuda_is_available()) "cuda" else "cpu"
+
+  model <- torchvision::model_alexnet(pretrained = pretrained, num_classes = num_classes)
+  # model$to(device = device)
+
+  # FIXME: Set lr, or maybe default is okay when using scheduler anyway
+  optimizer <- switch(optimizer,
+    "adam" = torch::optim_adam(model$parameters)
+  )
+
+  # FIXME: Parameterize gamma? Value copied verbatim from example docs
+  scheduler <- torch::lr_step(optimizer, step_size = step_size, gamma = 0.95)
+
+  loss_fn <- switch (loss,
+    "cross_entropy" = torch::nn_cross_entropy_loss()
+  )
+
+  # Training loop -----------------------------------------------------------
+
+  train_step <- function(batch) {
+    optimizer$zero_grad()
+    output <- model(batch[[1]]$to(device = device))
+    loss <- loss_fn(output, batch[[2]]$to(device = device))
+    loss$backward()
+    optimizer$step()
+    loss
+  }
+
+  valid_step <- function(batch) {
+    model$eval()
+    pred <- model(batch[[1]]$to(device = device))
+    # Example code had $add(1) after topk presumably b/c 0-indexed in old versions?
+    # k = 5 from example code
+    pred <- torch::torch_topk(pred, k = 5, dim = 2, TRUE, TRUE)[[2]]
+    pred <- pred$to(device = torch::torch_device("cpu"))
+    correct <- batch[[2]]$view(c(-1, 1))$eq(pred)$any(dim = 2)
+    model$train()
+    correct$to(dtype = torch::torch_float32())$mean()$item()
+  }
+
+  for (epoch in seq_len(epochs)) {
+
+    if (verbose) {
+    pb <- progress::progress_bar$new(
+      total = length(train_dl),
+      format = "[:bar] :eta Loss: :loss"
+    )
+    }
+
+    # FIXME: Probably should pre-allocate loss / acc vectors at some point
+    l <- c()
+    coro::loop(for (b in train_dl) {
+      loss <- train_step(b)
+      l <- c(l, loss$item())
+      if (verbose) pb$tick(tokens = list(loss = mean(l)))
+    })
+
+    acc <- c()
+    torch::with_no_grad({
+      coro::loop(for (b in valid_dl) {
+        accuracy <- valid_step(b)
+        acc <- c(acc, accuracy)
+      })
+    })
+
+    scheduler$step()
+    if (verbose) mlr3misc::catf("[epoch %d]: Loss = %3f.2, Acc= %3f.2 \n", epoch, mean(l), mean(acc))
+  }
+
+  # Return model object after training loop
+  return(model)
+}
---
+++ R/image-task.R
@@ -0,0 +1,130 @@
+#' Create a dataframe from a directory with the imagenet
+#' directory structure.
+#'
+#' The imagenet directory structure is laid out as follows
+#' Top-level dirs: `train`, `test`, `valid` \cr
+#' (you should provide path to those dirs as input) \cr
+#' Mid-level dirs: `class1`, `class2`, `...` \cr
+#' One directory for each class. The folders directly contain the images.
+#' @param dirs [`character`]\cr
+#'   List of dirs to create dataframes from.
+#' @return [`data.table`] with columns "uri" and "target" (the class).
+#' Column "uri" has additional class "imageuri".
+#' @export
+#' @examples
+#' \dontrun{
+#' df_from_imagenet_dir(c(
+#'   "/opt/example-data/imagenette2-160/train/",
+#'   "/opt/example-data/imagenette2-160/val/"
+#' ))
+#'
+#' # Only supplying the parent folder also works
+#' df_from_imagenet_dir(
+#'   "/opt/example-data/imagenette2-160/"
+#' )
+#' }
+df_from_imagenet_dir <- function(dirs) {
+
+  # known / valid image extensions to avoid e.g. spurious csv files
+  img_ext <- c("jpg", "jpeg", "png")
+
+  img_file <- fs::dir_ls(dirs, recurse = TRUE, type = "file")
+
+  # Subset to valid image files (by extension at least)
+  img_file <- img_file[which(tolower(fs::path_ext(img_file)) %in% img_ext)]
+
+  img_class <- fs::path_file(fs::path_dir(img_file))
+  class(img_file) <- c("imageuri", "character")
+
+  data.table::data.table(
+    target = factor(img_class),
+    uri = img_file
+  )
+}
+
+
+#' Create a torch dataset from a data.table of images
+#'
+#' @param df Table of image URIs and targets to create dataset from, as
+#'   returned by e.g. `df_from_imagenet_dir()`. Assumed to have columns
+#'   `'target'` and `'uri'`.
+#' @param row_ids Integer vector specifying which rows of `df` to use.
+#' @param transform Function of transformation to apply to input images.
+#'    By default applies `torchvision::transform_to_tensor()` only, yet
+#'    model-specific transformations (e.g. resizing) or data augmentation
+#'    may be necessary.
+#' @param target_transform Function of optional transformations applied to the
+#'   `'target'`, which will be coerced via `as.integer()` and
+#'   `torch::torch_tensor()` before any `target_transform()` is applied.
+#' @export
+#' @examples
+#' \dontrun{
+#' # Dataset of all imagenette160 images
+#' image_dt <- df_from_imagenet_dir("/opt/example-data/imagenette2-160/")
+#' img_ds   <- img_dataset(image_dt)
+#'
+#' # Datasets for train- and validation images
+#' # Inferring set from file path (URI)
+#' train_ids <- grepl("/train/", image_dt$uri)
+#' val_ids   <- grepl("/val/", image_dt$uri)
+#'
+#' img_train_ds <- img_dataset(image_dt, row_ids = train_ids)
+#' img_val_ds   <- img_dataset(image_dt, row_ids = val_ids)
+#'
+#' # Adding transformations, including device placement
+#' device <- if(torch::cuda_is_available()) "cuda" else "cpu"
+#'
+#' to_device <- function(x, device) {
+#'   x$to(device = device)
+#' }
+#'
+#' img_transform <- function(x) {
+#'   x %>%
+#'     torchvision::transform_to_tensor() %>%
+#'     to_device(device) %>%
+#'     torchvision::transform_resize(c(64, 64))
+#' }
+#'
+#' img_train_ds <- img_dataset(image_dt, row_ids = train_ids, img_transform)
+#' img_val_ds   <- img_dataset(image_dt, row_ids = val_ids, img_transform)
+#' }
+img_dataset <- torch::dataset(
+  "image-dataset",
+
+  # Input is the result of df_from_imagenet_dir()
+  # classes are converted to integers since they are stored as factors
+  # which should make class id <-> name conversion easy(ish)
+  initialize = function(df, row_ids = NULL,
+                        transform = torchvision::transform_to_tensor,
+                        target_transform = NULL) {
+
+    if (!is.null(row_ids)) df <- df[row_ids, ]
+
+    self$num_classes <- length(unique(df[["target"]]))
+
+    self$uri <- df[["uri"]]
+    self$target <- torch::torch_tensor(as.integer(df[["target"]]))
+    self$transform <- transform
+    self$target_transform <- target_transform
+  },
+
+  # Get individual item based on index only
+  .getitem = function(index) {
+
+    # target <- torch_tensor(self$class[index])
+    target <- as.integer(self$target[index])
+    img <- torchvision::magick_loader(self$uri[[index]])
+
+    if (!is.null(self$transform)) img <- self$transform(img)
+    if (!is.null(self$target_transform)) target <- self$target_transform(target)
+
+    list(x = img, y = target)
+  },
+
+  # It's optional, but helpful to define the .length method returning
+  # the number of elements in the dataset. This is needed if you want
+  # to shuffle your dataset.
+  .length = function() {
+    length(self$target)
+  }
+)
