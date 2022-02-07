cc3600309c608106e1b7d3862341b57f96f3c5fc
Lukas Burk <burk@leibniz-bips.de>
2022-02-07 17:48:52 +0100

    Expand docs

--- R/utils-models.R
+++ R/utils-models.R
@@ -2,6 +2,9 @@
 #'
 #' Used for pretrained models where the last layer is set to e.g. 1000 classes
 #' but training is intended to be applied to e.g. 10 classes.
+#'
+#' @note As of this, this also freezes the parameters of all but the last layer.
+#'
 #' @rdname reset-layer
 #' @param model A pretrained model, e.g.
 #' `torchvision::model_alexnet(pretrained = TRUE)`
@@ -13,10 +16,17 @@
 #'
 #' @examples
 #' \dontrun{
+#' # AlexNet
 #' model <- torchvision::model_alexnet(pretrained = TRUE)
 #' model$classifier[[7]]$out_feature
 #' model <- reset_last_layer(model, 10)
 #' model$classifier[[7]]$out_feature
+#'
+#' # ResNet
+#' model <- torchvision::model_resnet18(pretrained = TRUE)
+#' model$fc$out_feature
+#' model <- reset_last_layer(model, 10)
+#' model$fc$out_feature
 #' }
 reset_last_layer <- function(model, num_classes, bias = TRUE) {
   UseMethod("reset_last_layer")
@@ -33,7 +43,7 @@ reset_last_layer.AlexNet <- function(model, num_classes, bias = TRUE) {
 
   model$classifier$`6` <- torch::nn_linear(
     in_features = model$classifier$`6`$in_features,
-    out_features = num_classes, 
+    out_features = num_classes,
     bias = bias
   )
   return(model)
