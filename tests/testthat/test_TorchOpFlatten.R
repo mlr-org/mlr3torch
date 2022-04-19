54903e08862ed879c934de15ac165f4efc4f2987
Sebastian Fischer <sebf.fischer@gmail.com>
2022-04-19 19:33:09 +0200

    fixed flatten test

--- tests/testthat/test_TorchOpFlatten.R
+++ tests/testthat/test_TorchOpFlatten.R
@@ -2,7 +2,7 @@ test_that("TorchOpFlatten works", {
   task = tsk("boston_housing")
   inputs = list(input = torch_randn(10, 3, 4))
   to = top("flatten")
-  to$build(inputs, task, NULL)
+  layer = to$build(inputs, task, NULL)
   output = layer(tensor)
   expect_equal(output$shape, c(10L, 12L))
 })
