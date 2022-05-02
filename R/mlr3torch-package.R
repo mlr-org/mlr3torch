cf1d82202d3b99f17b9651d80c6a44decddd675d
Sebastian Fischer <sebf.fischer@gmail.com>
2022-05-02 09:35:50 +0200

    chore: unify with other mlr3 packages

--- R/mlr3torch-package.R
+++ R/mlr3torch-package.R
@@ -1,14 +1,1 @@
-#' @keywords internal
-"_PACKAGE"
 
-# The following block is used by usethis to automatically manage
-# roxygen namespace tags. Modify with care!
-## usethis namespace: start
-#' @import paradox
-#' @import mlr3misc
-#' @import mlr3
-#' @import checkmate
-#' @import data.table
-#' @importFrom R6 R6Class
-## usethis namespace: end
-NULL
--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,12 @@
+#' @keywords internal
+"_PACKAGE"
+
 #' @import paradox
-#' @import R6
+#' @import mlr3
+#' @import checkmate
+#' @import data.table
+#' @import mlr3misc
+#' @importFrom R6 R6Class
 #' @import torch
 #' @import luz
 #' @import mlr3pipelines
@@ -22,18 +29,26 @@ torch_reflections = new.env(parent = emptyenv())
 
 register_mlr3 = function() {
 
+
   # Learners ----------------------------------------------------------------
 
   lrns = utils::getFromNamespace("mlr_learners", ns = "mlr3")
   tsks = utils::getFromNamespace("mlr_tasks", ns = "mlr3")
 
+  tsks$add("tiny_imagenet", load_task_tiny_imagenet)
+
   # classification learners
-  lrns$add("classif.torch.tabnet", LearnerClassifTabNet)
-  lrns$add("classif.torch.alexnet", LearnerClassifTorchAlexNet)
+  lrns$add("classif.tabnet", LearnerClassifTabNet)
+  lrns$add("classif.alexnet", LearnerClassifAlexNet)
   lrns$add("classif.torch", LearnerClassifTorch)
 
   # regression learners
   # lrns$add("regr.torch.tabnet", LearnerClassifTabNet)
+
+  # PipeOps
+  mlr_pipeops = mlr3pipelines::mlr_pipeops
+  mlr_pipeops$add("imagetrafo", PipeOpImageTrafo)
+
 
 
   # Reflections -------------------------------------------------------------
@@ -63,11 +78,46 @@ register_mlr3 = function() {
       "prelu", "relu", "relu6", "rrelu", "selu", "sigmoid",
       "softplus", "softshrink", "softsign", "tanh", "tanhshrink", "threshold", "glu"
     )
+
+    torch_reflections$image_trafos = c(
+      "random_crop",
+      "center_crop",
+      "hflip",
+      "adjust_gamma",
+      "random_order",
+      "adjust_brightness",
+      "pad",
+      "random_affine",
+      "affine",
+      "random_rotation",
+      "vflip",
+      "random_resized_crop",
+      "crop",
+      "resized_crop",
+      "random_choice",
+      "resize",
+      "rgb_to_grayscale",
+      "adjust_saturation",
+      "linear_transformation",
+      "random_vertical_flip",
+      "random_horizontal_flip",
+      "color_jitter",
+      "adjust_contrast",
+      "rotate",
+      "adjust_hue",
+      "normalize",
+      "random_apply",
+      "to_tensor"
+    )
   })
 
 }
 
 .onLoad = function(libname, pkgname) {
+  # For caching directory
+  backports::import(pkgname)
+  backports::import(pkgname, "R_user_dir", force = TRUE)
+
   register_namespace_callback(pkgname, "mlr3", register_mlr3)
   assign("lg", lgr::get_logger(pkgname), envir = parent.env(environment()))
   if (Sys.getenv("IN_PKGDOWN") == "true") {
