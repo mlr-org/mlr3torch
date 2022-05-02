304bc20a4f346dfa12be2c99d8bd643df454c7ca
Sebastian Fischer <sebf.fischer@gmail.com>
2022-05-02 09:28:43 +0200

    feat: provide toy task in inst

---
+++ inst/toytask.R
@@ -0,0 +1,16 @@
+library(mlr3torch)
+task = tsk("tiny_imagenet")
+dat = task$data()
+idx = seq(from = 1, by = 500, length.out = 200)
+dat = dat[idx, ]
+print(length(unique(dat$class)))
+uris = dat$image
+
+for (uri in uris) {
+  command = sprintf("cp %s ~/mlr/mlr3torch/inst/toytask/", uri)
+  system(
+    command
+  )
+}
+
+labels = dat$class
