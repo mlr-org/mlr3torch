528561299321e633c5f382edc789a7c43c975879
Sebastian Fischer <sebf.fischer@gmail.com>
2023-05-08 18:35:49 +0200

    typo

--- man-roxygen/pipeop_torch.R
+++ man-roxygen/pipeop_torch.R
@@ -4,4 +4,4 @@
 #' The state is the value calculated by the public method `$shapes_out()`.
 #' @family PipeOps
 #' @section Credit:
-#' Part of this documentation have been copied or adapted from the documentation of /CRANpkg{torch}.
+#' Part of this documentation have been copied or adapted from the documentation of \CRANpkg{torch}.
