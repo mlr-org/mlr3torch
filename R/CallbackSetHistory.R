102b5e14df7b4a2f5eaf38b228429bc73517d3f6
Sebastian Fischer <sebf.fischer@gmail.com>
2023-07-27 17:42:59 +0200

    fix tests

--- R/CallbackSetHistory.R
+++ R/CallbackSetHistory.R
@@ -70,7 +70,6 @@ CallbackSetHistory = R6Class("CallbackSetHistory",
         stopf("No eligible measures to plot for set '%s'.", set)
       }
 
-
       epoch = score = measure = NULL
       if (ncol(data) == 2L) {
         ggplot2::ggplot(data = data, ggplot2::aes(x = epoch, y = !!rlang::sym(measures))) +
@@ -84,7 +83,7 @@ CallbackSetHistory = R6Class("CallbackSetHistory",
           theme
       } else {
         data = melt(data, id.vars = "epoch", variable.name = "measure", value.name = "score")
-        ggplot2::ggplot(data = data, ggplot2::aes_string(x = epoch, y = score, color = measure)) +
+        ggplot2::ggplot(data = data, ggplot2::aes(x = epoch, y = score, color = measure)) +
           viridis::scale_color_viridis(discrete = TRUE) +
           ggplot2::geom_line() +
           ggplot2::geom_point() +
--- data-raw/tiny_imagenet.R
+++ data-raw/tiny_imagenet.R
@@ -2,4 +2,4 @@ devtools::load_all()
 
 dir = tempfile()
 ci = col_info(tsk("tiny_imagenet")$backend$backend)
-saveRDS(ci, "./data/col_info/tiny_imagenet.rds")
+saveRDS(ci, "./inst/col_info/tiny_imagenet.rds")
