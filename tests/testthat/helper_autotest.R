310ba2bff3b68d6a0e2624a44ed38fffe047bbb5
Sebastian Fischer <sebf.fischer@gmail.com>
2024-01-26 11:29:04 +0100

    fix test

--- tests/testthat/helper_autotest.R
+++ tests/testthat/helper_autotest.R
@@ -301,5 +301,5 @@ autotest_torch_callback = function(torch_callback, check_man = TRUE) {
   expect_class(cb_trained, "CallbackSet")
   expect_deep_clone(cb_trained, cb_trained$clone(deep = TRUE))
   cb_trained$ctx = "placeholder"
-  expect_error(cb_trained$clone(deep = TRUE), "must never be cloned unless")
+  expect_error(cb_trained$clone(deep = TRUE), "can only be cloned")
 }
