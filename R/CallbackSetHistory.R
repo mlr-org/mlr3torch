72a471bb796a31ed66413d708d878e6475b02836
Sebastian Fischer <sebf.fischer@gmail.com>
2024-02-26 16:45:09 +0100

    ...

--- R/CallbackSetHistory.R
+++ R/CallbackSetHistory.R
@@ -21,9 +21,16 @@ CallbackSetHistory = R6Class("CallbackSetHistory",
     },
     #' @description
     #' Converts the lists to data.tables.
-    on_end = function() {
-      self$train = rbindlist(self$train, fill = TRUE)
-      self$valid = rbindlist(self$valid, fill = TRUE)
+    state_dict = function() {
+      structure(list(
+        train = rbindlist(self$train, fill = TRUE),
+        valid = rbindlist(self$valid, fill = TRUE)
+      ), class = "learner_torch_history")
+    },
+    load_state_dict = function(state_dict) {
+      assert_class(state_dict, "learner_torch_history")
+      self$train = state_dict$train
+      self$valid = state_dict$valid
     },
     #' @description
     #' Add the latest training scores to the history.
@@ -41,58 +48,6 @@ CallbackSetHistory = R6Class("CallbackSetHistory",
         self$valid[[length(self$valid) + 1]] = c(
           list(epoch = self$ctx$epoch), self$ctx$last_scores_valid
         )
-      }
-    },
-    #' @description Plots the history.
-    #' @param measures (`character()`)\cr
-    #'   Which measures to plot. No default.
-    #' @param set (`character(1)`)\cr
-    #'   Which set to plot. Either `"train"` or `"valid"`. Default is `"valid"`.
-    #' @param epochs (`integer()`)\cr
-    #'   An integer vector restricting which epochs to plot. Default is `NULL`, which plots all epochs.
-    #' @param theme ([ggplot2::theme()])\cr
-    #'   The theme, [ggplot2::theme_minimal()] is the default.
-    #' @param ... (any)\cr
-    #'   Currently unused.
-    plot = function(measures, set = "valid", epochs = NULL, theme = ggplot2::theme_minimal(), ...) {
-      assert_choice(set, c("valid", "train"))
-      data = self[[set]]
-      assert_subset(measures, colnames(data))
-
-      if (is.null(epochs)) {
-        data = data[, c("epoch", measures), with = FALSE]
-      } else {
-        assert_integerish(epochs, unique = TRUE)
-        data = data[get("epoch") %in% epochs, c("epoch", measures), with = FALSE]
-      }
-
-      if ((!nrow(data)) || (ncol(data) < 2)) {
-        stopf("No eligible measures to plot for set '%s'.", set)
-      }
-
-      epoch = score = measure = .data = NULL
-      if (ncol(data) == 2L) {
-        ggplot2::ggplot(data = data, ggplot2::aes(x = epoch, y = .data[[measures]])) +
-          ggplot2::geom_line() +
-          ggplot2::geom_point() +
-          ggplot2::labs(
-            x = "Epoch",
-            y = measures,
-            title = sprintf("%s Loss", switch(set, valid = "Validation", train = "Training"))
-          ) +
-          theme
-      } else {
-        data = melt(data, id.vars = "epoch", variable.name = "measure", value.name = "score")
-        ggplot2::ggplot(data = data, ggplot2::aes(x = epoch, y = score, color = measure)) +
-          viridis::scale_color_viridis(discrete = TRUE) +
-          ggplot2::geom_line() +
-          ggplot2::geom_point() +
-          ggplot2::labs(
-            x = "Epoch",
-            y = "Score",
-            title = sprintf("%s Loss", switch(set, valid = "Validation", train = "Training"))
-          ) +
-          theme
       }
     }
   ),
