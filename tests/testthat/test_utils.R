cb29cc7ff90ef8a90f5a2cf8a59acfa48c7c35c0
Sebastian Fischer <sebf.fischer@gmail.com>
2024-06-17 10:18:00 +0200

    trigger ci again

--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -2,7 +2,6 @@ test_that("make_check_vector works", {
   check_vector1 = make_check_vector(1)
   expect_true(check_vector1(1))
   expect_equal(check_vector1(1:2), "Must be an integerish vector of length 1.")
-
   check_vector2 = make_check_vector(2)
   expect_true(check_vector2(1:2))
   expect_equal(check_vector2(1:3), "Must be an integerish vector of length 1 or 2.")
