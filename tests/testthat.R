26f3792d567bbfd44df29c10d6b4ad074b4139bb
Sebastian Fischer <sebf.fischer@gmail.com>
2024-08-14 09:36:32 +0200

    ...

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,6 +1,6 @@
-library(testthat)
-library(mlr3torch)
-
 if (identical(Sys.getenv("TORCH_TEST", unset = "0"), "1")) {
+  library("checkmate")
+  library("testthat")
+  library("mlr3torch")
   test_check("torch")
 }
