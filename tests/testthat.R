b451116b0143efcdb70a4f49de3fbbd287cd103c
Sebastian Fischer <sebf.fischer@gmail.com>
2024-08-14 09:37:13 +0200

    ...

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,5 +2,5 @@ if (identical(Sys.getenv("TORCH_TEST", unset = "0"), "1")) {
   library("checkmate")
   library("testthat")
   library("mlr3torch")
-  test_check("torch")
+  test_check("mlr3torch")
 }
