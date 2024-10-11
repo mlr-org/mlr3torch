43a8ffb4d9f48177600fed1df28846aaea46acb2
Carson Zhang <carsonzhang4@gmail.com>
2024-10-11 08:38:33 +0200

    removed magrittr pipe from tests

--- R/CallbackSetTB.R
+++ R/CallbackSetTB.R
@@ -14,63 +14,63 @@
 #' @export
 #' @include CallbackSet.R
 CallbackSetTB = R6Class("CallbackSetTB",
-    inherit = CallbackSet,
-    lock_objects = FALSE,
-    public = list(
-        #' @description
-        #' Creates a new instance of this [R6][R6::R6Class] class.
-        initialize = function(path) {
-          self$path = assert_path_for_output(path)
-          if (!dir.exists(path)) {
-            dir.create(path, recursive = TRUE)
-          }
-        },
-        #' @description
-        #' Logs the training loss and validation measures as TensorFlow events.
-        #' Meaningful changes happen at the end of each epoch.
-        #' Notably NOT on_batch_valid_end, since there are no gradient steps between validation batches,
-        #' and therefore differences are due to randomness
-        # TODO: display the appropriate x axis with its label in TensorBoard
-        # relevant when we log different scores at different times
-        on_epoch_end = function() {
-            private$log_train_score()
+  inherit = CallbackSet,
+  lock_objects = FALSE,
+  public = list(
+    #' @description
+    #' Creates a new instance of this [R6][R6::R6Class] class.
+    initialize = function(path) {
+      self$path = assert_path_for_output(path)
+      if (!dir.exists(path)) {
+        dir.create(path, recursive = TRUE)
+      }
+    },
+    #' @description
+    #' Logs the training loss and validation measures as TensorFlow events.
+    #' Meaningful changes happen at the end of each epoch.
+    #' Notably NOT on_batch_valid_end, since there are no gradient steps between validation batches,
+    #' and therefore differences are due to randomness
+    # TODO: display the appropriate x axis with its label in TensorBoard
+    # relevant when we log different scores at different times
+    on_epoch_end = function() {
+      private$log_train_score()
 
-            if (length(self$ctx$last_scores_valid)) {
-                map(names(self$ctx$measures_valid), private$log_valid_score)
-            }
-        }
-    ),
-    private = list(
-      # TODO: refactor into a single function with the following signature
-        # log_score = function(prefix, measure_name, score) {
-        #
-        # },
-        log_valid_score = function(measure_name) {
-          valid_score = list(self$ctx$last_scores_valid[[measure_name]])
-          names(valid_score) = paste0("valid.", measure_name)
-          with_logdir(self$path, {
-            do.call(log_event, valid_score)
-          })
-        },
-        log_train_score = function() {
-          # TODO: figure out what self$ctx$last_loss looks like when there are multiple train measures
-          # TODO: remind ourselves why we wanted to display last_loss and not last_scores_train
-          with_logdir(self$path, {
-            log_event(train.loss = self$ctx$last_loss)
-          })
-        }
-    )
+      if (length(self$ctx$last_scores_valid)) {
+        map(names(self$ctx$measures_valid), private$log_valid_score)
+      }
+    }
+  ),
+  private = list(
+    # TODO: refactor into a single function with the following signature
+    # log_score = function(prefix, measure_name, score) {
+    #
+    # },
+    log_valid_score = function(measure_name) {
+      valid_score = list(self$ctx$last_scores_valid[[measure_name]])
+      names(valid_score) = paste0("valid.", measure_name)
+      with_logdir(self$path, {
+        do.call(log_event, valid_score)
+      })
+    },
+    log_train_score = function() {
+      # TODO: figure out what self$ctx$last_loss looks like when there are multiple train measures
+      # TODO: remind ourselves why we wanted to display last_loss and not last_scores_train
+      with_logdir(self$path, {
+        log_event(train.loss = self$ctx$last_loss)
+      })
+    }
+  )
 )
 
 
 mlr3torch_callbacks$add("tb", function() {
-    TorchCallback$new(
-        callback_generator = CallbackSetTB,
-        param_set = ps(
-            path      = p_uty(tags = c("train", "required"))
-        ),
-        id = "tb",
-        label = "TensorBoard",
-        man = "mlr3torch::mlr_callback_set.tb"
-    )
+  TorchCallback$new(
+    callback_generator = CallbackSetTB,
+    param_set = ps(
+      path      = p_uty(tags = c("train", "required"))
+    ),
+    id = "tb",
+    label = "TensorBoard",
+    man = "mlr3torch::mlr_callback_set.tb"
+  )
 })
--- tests/testthat/test_CallbackSetTB.R
+++ tests/testthat/test_CallbackSetTB.R
@@ -1,52 +1,45 @@
 library(tfevents)
 
 test_that("basic", {
-    cb = t_clbk("tb", path = tempfile())
-    expect_torch_callback(cb, check_man = FALSE)
+  cb = t_clbk("tb", path = tempfile())
+  expect_torch_callback(cb, check_man = FALSE)
 })
 
 # TODO: investigate what's happening when there is only a single epoch (why don't we log anything?)
 test_that("a simple example works", {
-    # using a temp dir
-    cb = t_clbk("tb")
+  # using a temp dir
+  cb = t_clbk("tb")
 
-    task = tsk("iris")
-    n_epochs = 10
-    batch_size = 50
-    neurons = 200
+  task = tsk("iris")
+  n_epochs = 10
+  batch_size = 50
+  neurons = 200
 
-    pth0 = tempfile()
+  pth0 = tempfile()
 
-    mlp = lrn("classif.mlp",
-          callbacks = cb,
-          epochs = n_epochs, batch_size = batch_size, neurons = neurons,
-          validate = 0.2,
-          measures_valid = msrs(c("classif.acc", "classif.ce")),
-          measures_train = msrs(c("classif.acc", "classif.ce"))
-    )
-    mlp$param_set$set_values(cb.tb.path = pth0)
+  mlp = lrn("classif.mlp",
+            callbacks = cb,
+            epochs = n_epochs, batch_size = batch_size, neurons = neurons,
+            validate = 0.2,
+            measures_valid = msrs(c("classif.acc", "classif.ce")),
+            measures_train = msrs(c("classif.acc", "classif.ce"))
+  )
+  mlp$param_set$set_values(cb.tb.path = pth0)
 
-    mlp$train(task)
+  mlp$train(task)
 
-    events = collect_events(pth0)$summary %>%
-        mlr3misc::map(unlist)
+  events = mlr3misc::map(collect_events(pth0)$summary, unlist)
 
-    n_last_loss_events = mlr3misc::map(events, \(x) x["tag"] == "train.loss") %>%
-        unlist() %>%
-        sum()
+  n_last_loss_events = sum(unlist(mlr3misc::map(events, \(x) x["tag"] == "train.loss")))
 
-    n_valid_acc_events = mlr3misc::map(events, \(x) x["tag"] == "valid.classif.acc") %>%
-      unlist() %>%
-      sum()
+  n_valid_acc_events = sum(unlist(mlr3misc::map(events, \(x) x["tag"] == "valid.classif.acc")))
 
-    n_valid_ce_events = mlr3misc::map(events, \(x) x["tag"] == "valid.classif.ce") %>%
-      unlist() %>%
-      sum()
+  n_valid_ce_events = sum(unlist(mlr3misc::map(events, \(x) x["tag"] == "valid.classif.ce")))
 
-    # TODO: refactor to expect a specific ordering of the events list
-    expect_equal(n_last_loss_events, n_epochs)
-    expect_equal(n_valid_acc_events, n_epochs)
-    expect_equal(n_valid_ce_events, n_epochs)
+  # TODO: refactor to expect a specific ordering of the events list
+  expect_equal(n_last_loss_events, n_epochs)
+  expect_equal(n_valid_acc_events, n_epochs)
+  expect_equal(n_valid_ce_events, n_epochs)
 })
 
 test_that("eval_freq works", {
@@ -73,20 +66,13 @@ test_that("eval_freq works", {
 
   mlp$train(task)
 
-  events = collect_events(pth0)$summary %>%
-    mlr3misc::map(unlist)
+  events = mlr3misc::map(collect_events(pth0)$summary, unlist)
 
-  n_last_loss_events = mlr3misc::map(events, \(x) x["tag"] == "train.loss") %>%
-    unlist() %>%
-    sum()
+  n_last_loss_events = sum(unlist(mlr3misc::map(events, \(x) x["tag"] == "train.loss")))
 
-  n_valid_acc_events = mlr3misc::map(events, \(x) x["tag"] == "valid.classif.acc") %>%
-    unlist() %>%
-    sum()
+  n_valid_acc_events = sum(unlist(mlr3misc::map(events, \(x) x["tag"] == "valid.classif.acc")))
 
-  n_valid_ce_events = mlr3misc::map(events, \(x) x["tag"] == "valid.classif.ce") %>%
-    unlist() %>%
-    sum()
+  n_valid_ce_events = sum(unlist(mlr3misc::map(events, \(x) x["tag"] == "valid.classif.ce")))
 
   expect_equal(n_last_loss_events, n_epochs)
   expect_equal(n_valid_acc_events, ceiling(n_epochs / 4))
