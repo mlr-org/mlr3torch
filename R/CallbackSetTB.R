7c9f431d3e368d838ec081d0fa37d15dd33831fb
Carson Zhang <carsonzhang4@gmail.com>
2024-10-11 17:37:06 +0200

    all tests pass, I think this is parity with the previous broken commit. still need to incorporate the step logging

--- R/CallbackSetTB.R
+++ R/CallbackSetTB.R
@@ -10,6 +10,8 @@
 #' @param path (`character(1)`)\cr
 #'   The path to a folder where the events are logged.
 #'   Point TensorBoard to this folder to view them.
+#' @param log_train_loss (`logical(1)`)\cr
+#'  Whether we log the training loss.
 #' @family Callback
 #' @export
 #' @include CallbackSet.R
@@ -19,11 +21,12 @@ CallbackSetTB = R6Class("CallbackSetTB",
   public = list(
     #' @description
     #' Creates a new instance of this [R6][R6::R6Class] class.
-    initialize = function(path) {
+    initialize = function(path, log_train_loss) {
       self$path = assert_path_for_output(path)
       if (!dir.exists(path)) {
         dir.create(path, recursive = TRUE)
       }
+      self$log_train_loss = assert_logical(log_train_loss)
     },
     #' @description
     #' Logs the training loss and validation measures as TensorFlow events.
@@ -33,26 +36,35 @@ CallbackSetTB = R6Class("CallbackSetTB",
     # TODO: display the appropriate x axis with its label in TensorBoard
     # relevant when we log different scores at different times
     on_epoch_end = function() {
-      private$log_train_score()
+      if (self$log_train_loss) {
+        private$.log_train_loss()
+      }
+
+      if (length(self$ctx$last_scores_train)) {
+        map(names(self$ctx$measures_train), private$.log_train_score)
+      }
 
       if (length(self$ctx$last_scores_valid)) {
-        map(names(self$ctx$measures_valid), private$log_valid_score)
+        map(names(self$ctx$measures_valid), private$.log_valid_score)
       }
     }
   ),
   private = list(
-    # TODO: refactor into a single function with the following signature
-    # log_score = function(prefix, measure_name, score) {
-    #
-    # },
-    log_valid_score = function(measure_name) {
+    .log_valid_score = function(measure_name) {
       valid_score = list(self$ctx$last_scores_valid[[measure_name]])
       names(valid_score) = paste0("valid.", measure_name)
       with_logdir(self$path, {
         do.call(log_event, valid_score)
       })
     },
-    log_train_score = function() {
+    .log_train_score = function(measure_name) {
+      train_score = list(self$ctx$last_scores_train[[measure_name]])
+      names(train_score) = paste0("train.", measure_name)
+      with_logdir(self$path, {
+        do.call(log_event, train_score)
+      })
+    },
+    .log_train_loss = function() {
       # TODO: remind ourselves why we wanted to display last_loss and not last_scores_train
       with_logdir(self$path, {
         log_event(train.loss = self$ctx$last_loss)
@@ -61,12 +73,13 @@ CallbackSetTB = R6Class("CallbackSetTB",
   )
 )
 
-
+#' @include TorchCallback.R
 mlr3torch_callbacks$add("tb", function() {
   TorchCallback$new(
     callback_generator = CallbackSetTB,
     param_set = ps(
-      path      = p_uty(tags = c("train", "required"))
+      path           = p_uty(tags = c("train", "required")),
+      log_train_loss = p_lgl(tags = c("train", "required"))
     ),
     id = "tb",
     label = "TensorBoard",
--- tests/testthat/test_CallbackSetTB.R
+++ tests/testthat/test_CallbackSetTB.R
@@ -1,8 +1,8 @@
 library(tfevents)
 
-test_that("basic", {
-  cb = t_clbk("tb", path = tempfile())
-  expect_torch_callback(cb, check_man = FALSE)
+test_that("autotest", {
+  cb = t_clbk("tb", path = tempfile(), log_train_loss = TRUE)
+  expect_torch_callback(cb, check_man = TRUE)
 })
 
 # TODO: investigate what's happening when there is only a single epoch (why don't we log anything?)
@@ -17,6 +17,8 @@ test_that("a simple example works", {
 
   pth0 = tempfile()
 
+  log_train_loss = TRUE
+
   mlp = lrn("classif.mlp",
             callbacks = cb,
             epochs = n_epochs, batch_size = batch_size, neurons = neurons,
@@ -26,16 +28,26 @@ test_that("a simple example works", {
   )
   mlp$param_set$set_values(cb.tb.path = pth0)
 
+  mlp$param_set$set_values(cb.tb.log_train_loss = log_train_loss)
+
   mlp$train(task)
 
   events = mlr3misc::map(collect_events(pth0)$summary, unlist)
 
-  n_last_loss_events = sum(unlist(mlr3misc::map(events, \(x) x["tag"] == "train.loss")))
-  n_valid_acc_events = sum(unlist(mlr3misc::map(events, \(x) x["tag"] == "valid.classif.acc")))
-  n_valid_ce_events = sum(unlist(mlr3misc::map(events, \(x) x["tag"] == "valid.classif.ce")))
+  event_tag_is = function(event, tag_name) {
+    ifelse(is.null(event), FALSE, event["tag"] == tag_name)
+  }
+
+  n_train_loss_events = sum(unlist(mlr3misc::map(events, event_tag_is, tag_name = "train.loss")))
+  n_train_acc_events = sum(unlist(mlr3misc::map(events, event_tag_is, tag_name = "train.classif.acc")))
+  n_train_ce_events = sum(unlist(mlr3misc::map(events, event_tag_is, tag_name = "train.classif.ce")))
+  n_valid_acc_events = sum(unlist(mlr3misc::map(events, event_tag_is, tag_name = "valid.classif.acc")))
+  n_valid_ce_events = sum(unlist(mlr3misc::map(events, event_tag_is, tag_name = "valid.classif.ce")))
 
   # TODO: refactor to expect a specific ordering of the events list, not just the right counts
-  expect_equal(n_last_loss_events, n_epochs)
+  expect_equal(n_train_loss_events, n_epochs)
+  expect_equal(n_train_acc_events, n_epochs)
+  expect_equal(n_train_ce_events, n_epochs)
   expect_equal(n_valid_acc_events, n_epochs)
   expect_equal(n_valid_ce_events, n_epochs)
 })
@@ -52,6 +64,8 @@ test_that("eval_freq works", {
 
   pth0 = tempfile()
 
+  log_train_loss = TRUE
+
   mlp = lrn("classif.mlp",
             callbacks = cb,
             epochs = n_epochs, batch_size = batch_size, neurons = neurons,
@@ -61,23 +75,63 @@ test_that("eval_freq works", {
             eval_freq = eval_freq
   )
   mlp$param_set$set_values(cb.tb.path = pth0)
+  mlp$param_set$set_values(cb.tb.log_train_loss = log_train_loss)
 
   mlp$train(task)
 
   events = mlr3misc::map(collect_events(pth0)$summary, unlist)
 
-  n_last_loss_events = sum(unlist(mlr3misc::map(events, \(x) x["tag"] == "train.loss")))
-  n_valid_acc_events = sum(unlist(mlr3misc::map(events, \(x) x["tag"] == "valid.classif.acc")))
-  n_valid_ce_events = sum(unlist(mlr3misc::map(events, \(x) x["tag"] == "valid.classif.ce")))
+  event_tag_is = function(event, tag_name) {
+    ifelse(is.null(event), FALSE, event["tag"] == tag_name)
+  }
 
-  expect_equal(n_last_loss_events, n_epochs)
-  expect_equal(n_valid_acc_events, ceiling(n_epochs / 4))
-  expect_equal(n_valid_ce_events, ceiling(n_epochs / 4))
+  n_train_loss_events = sum(unlist(mlr3misc::map(events, event_tag_is, tag_name = "train.loss")))
+  n_train_acc_events = sum(unlist(mlr3misc::map(events, event_tag_is, tag_name = "train.classif.acc")))
+  n_train_ce_events = sum(unlist(mlr3misc::map(events, event_tag_is, tag_name = "train.classif.ce")))
+  n_valid_acc_events = sum(unlist(mlr3misc::map(events, event_tag_is, tag_name = "valid.classif.acc")))
+  n_valid_ce_events = sum(unlist(mlr3misc::map(events, event_tag_is, tag_name = "valid.classif.ce")))
+
+  expect_equal(n_train_loss_events, n_epochs)
+  expect_equal(n_train_acc_events, ceiling(n_epochs / eval_freq))
+  expect_equal(n_train_ce_events, ceiling(n_epochs / eval_freq))
+  expect_equal(n_valid_acc_events, ceiling(n_epochs / eval_freq))
+  expect_equal(n_valid_ce_events, ceiling(n_epochs / eval_freq))
+})
+
+test_that("the flag for tracking the train loss works", {
+  cb = t_clbk("tb")
+
+  task = tsk("iris")
+  n_epochs = 10
+  batch_size = 50
+  neurons = 200
+
+  log_train_loss = FALSE
+
+  pth0 = tempfile()
+
+  mlp = lrn("classif.mlp",
+            callbacks = cb,
+            epochs = n_epochs, batch_size = batch_size, neurons = neurons,
+            validate = 0.2,
+            measures_valid = msrs(c("classif.acc", "classif.ce")),
+            measures_train = msrs(c("classif.acc", "classif.ce"))
+  )
+  mlp$param_set$set_values(cb.tb.path = pth0)
+  mlp$param_set$set_values(cb.tb.log_train_loss = log_train_loss)
+
+  mlp$train(task)
+
+  events = mlr3misc::map(collect_events(pth0)$summary, unlist)
+
+  n_train_loss_events = sum(unlist(mlr3misc::map(events, event_tag_is, tag_name = "train.loss")))
+
+  expect_equal(n_train_loss_events, 0)
 })
 
 test_that("throws an error when using existing directory", {
   path = tempfile()
   dir.create(path)
-  cb = t_clbk("tb", path = path)
+  cb = t_clbk("tb", path = path, log_train_loss = TRUE)
   expect_error(cb$generate(), "already exists")
 })
