89a72f126ebcc35274bc1ea913493af3fb6e07fd
Carson Zhang <carsonzhang4@gmail.com>
2024-10-20 20:53:40 +0200

    added install script

--- benchmarks/rf_use_case/single_task.R
+++ benchmarks/rf_use_case/single_task.R
@@ -17,6 +17,22 @@ cc18_small = fread(here(getOption("mlr3oml.cache"), "collections", "cc18_small.c
 
 task_list = mlr3misc::pmap(cc18_small, function(data_id, name, NumberOfFeatures, NumberOfInstances) tsk("oml", data_id = data_id))
 
+task_list
+
+# define the learners
+# neurons = function(n_layers, latent_dim) {
+#   rep(latent_dim, n_layers)
+# }
+
+# n_layers_values <- 1:5
+# latent_dim_values <- seq(10, 200, by = 20)
+# neurons_search_space <- mapply(
+#   neurons,
+#   expand.grid(n_layers = n_layers_values, latent_dim = latent_dim_values)$n_layers,
+#   expand.grid(n_layers = n_layers_values, latent_dim = latent_dim_values)$latent_dim,
+#   SIMPLIFY = FALSE
+# )
+
 mlp = lrn("classif.mlp",
   activation = nn_relu,
   neurons = to_tune(ps(
@@ -25,6 +41,7 @@ mlp = lrn("classif.mlp",
       list(neurons = rep(x$latent, x$n_layers))
     })
   ),
+  # neurons = to_tune(neurons_search_space),
   batch_size = to_tune(c(64, 128, 256)),
   p = to_tune(0.1, 0.7),
   epochs = to_tune(upper = 1000L, internal = TRUE),
@@ -52,7 +69,7 @@ lrn_rf = lrn("classif.ranger")
 options(mlr3.exec_random = FALSE)
 
 design = benchmark_grid(
-  task_list,
+  task_list[[1]],
   learners = list(at, lrn_rf),
   resampling = rsmp("cv", folds = 3)
 )
