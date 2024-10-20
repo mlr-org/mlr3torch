ae5709c895e0d922ee2674c0c8aa9ff73b0f73a1
Carson Zhang <carsonzhang4@gmail.com>
2024-10-20 21:44:11 +0200

    added initial test file, script where I will interactively try out the task

---
+++ tests/testthat/test_TaskClassif_melanoma.R
@@ -0,0 +1,18 @@
+skip_on_cran()
+
+test_that("melanoma task works", {
+  withr::local_options(mlr3torch.cache = TRUE)
+  task = tsk("melanoma")
+  # this makes the test faster
+  # task$row_roles$use = 1:10
+  # expect_equal(task$id, "mnist")
+  # expect_equal(task$label, "MNIST Digit Classification")
+  # expect_equal(task$feature_names, "image")
+  # expect_equal(task$target_names, "label")
+  # expect_equal(task$man, "mlr3torch::mlr_tasks_mnist")
+  # expect_equal(task$properties, "multiclass")
+
+  # x = materialize(task$data(task$row_ids[1:2], cols = "image")[[1L]], rbind = TRUE)
+  # expect_equal(x$shape, c(2, 1, 28, 28))
+  # expect_equal(x$dtype, torch_float32())
+})
