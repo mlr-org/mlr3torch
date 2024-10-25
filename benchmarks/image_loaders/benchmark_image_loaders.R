1c0e12e7d100de00e96484a95b02a57e9e6113be
Carson Zhang <carsonzhang4@gmail.com>
2024-10-25 12:10:27 +0200

    benchmark code for image loaders

---
+++ benchmarks/image_loaders/benchmark_image_loaders.R
@@ -0,0 +1,62 @@
+library(torch)
+library(torchvision)
+library(mlr3torch)
+library(here)
+
+library(data.table)
+library(tidytable)
+
+training_metadata = fread(here::here("cache", "ISIC_2020_Training_GroundTruth.csv"))
+
+# hard-coded cache directory that I use locally
+cache_dir = here("cache")
+
+# construct a torch dataset
+ds_base_loader = torch::dataset(
+  initialize = function(n_images) {
+    self$.metadata = fread(here(cache_dir, "ISIC_2020_Training_GroundTruth.csv"))[1:n_images, ]
+    self$.path = file.path(here(cache_dir), "train")
+  },
+  .getitem = function(idx) {
+    force(idx)
+
+    x = torchvision::base_loader(file.path(self$.path, paste0(self$.metadata[idx, ]$image_name, ".jpg")))
+    x = torchvision::transform_to_tensor(x)
+
+    return(list(x = x))
+  },
+  .length = function() {
+    nrow(self$.metadata)
+  }
+)
+
+ds_magick_loader = torch::dataset(
+  initialize = function(n_images) {
+    self$.metadata = fread(here(cache_dir, "ISIC_2020_Training_GroundTruth.csv"))[1:n_images, ]
+    self$.path = file.path(here(cache_dir), "train")
+  },
+  .getitem = function(idx) {
+    force(idx)
+
+    image_name = self$.metadata[idx, ]$image_name
+
+    x = torchvision::magick_loader(file.path(self$.path, paste0(image_name, ".jpg")))
+    x = torchvision::transform_to_tensor(x)
+
+    return(list(x = x, image_name = image_name))
+  },
+  .length = function() {
+    nrow(self$.metadata)
+  }
+)
+
+n_images = 10
+
+ds_base = ds_base_loader(n_images)
+ds_magick = ds_magick_loader(n_images)
+
+bench::mark(
+  for (i in 1:n_images) ds_base$.getitem(i),
+  for (i in 1:n_images) ds_magick$.getitem(i),
+  memory = FALSE
+)
--- tests/temp-TaskClassif_melanoma.R
+++ tests/temp-TaskClassif_melanoma.R
@@ -15,7 +15,7 @@ cache_dir = here("cache")
 # construct a torch dataset
 ds = torch::dataset(
   initialize = function() {
-    self$.metadata = fread(here("cache", "ISIC_2020_Training_GroundTruth.csv"))
+    self$.metadata = fread(here(cache_dir, "ISIC_2020_Training_GroundTruth.csv"))
     self$.path = file.path(here(cache_dir), "train")
   },
   .getitem = function(idx) {
@@ -23,9 +23,9 @@ ds = torch::dataset(
 
     x = torchvision::base_loader(file.path(self$.path, paste0(self$.metadata[idx, ]$image_name, ".jpg")))
     # TODO: decide on these transformations
-    x = torchvision::transform_to_tensor(x) %>% torchvision::transform_rgb_to_grayscale()
+    x = torchvision::transform_to_tensor(x)
 
-    # TODO: should we only return the images here?
+    # TODO: should we only return the images here? I think yes
     return(list(x = x))
   },
   .length = function() {
@@ -35,18 +35,14 @@ ds = torch::dataset(
 
 melanoma_ds = ds()
 
-
+# dd_dims = c(NA, )
+dd = as_data_descriptor(melanoma_ds, list(x = dd_dims))
 
 # Construct lazy tensor for each image (e.g. a data table with a single ltnsr column)
-
-# Join with the metadata file
-
-# TODO: figure out the dimensions. The images each have a different dimension.
-# dd_dims = c(NA, )
-# dd = as_data_descriptor(ds, list(x = dd_dims))
-
+# TODO: confirm that this maintains the same ordering
 lt = lazy_tensor(dd)
 
+# Join with the metadata file
 dt_train = inner_join(training_metadata, data.table(..., x = lt), by = image_name)
 
 # as_task_regr(dt_train, target = "corr", id = "guess_the_correlation")
