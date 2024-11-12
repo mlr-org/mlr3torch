a2fbc2069c1bb7b42b74341d96ec2f10d6aac9e6
Carson Zhang <carsonzhang4@gmail.com>
2024-11-12 16:16:03 +0100

    added SelectorParam

--- R/CallbackSetUnfreeze.R
+++ R/CallbackSetUnfreeze.R
@@ -1,70 +1,70 @@
-# #' @title Unfreezing Weights Callback
-# #' 
-# #' @name mlr_callback_set.unfreeze
-# #' 
-# #' @description 
-# #' Unfreeze some weights after some number of steps or epochs.
-# #' 
-# #' @details 
-# #' TODO: add
-# #' 
-# #' @param starting_weights (`Selector`)\cr
-# #'  A `Selector` denoting the weights that are trainable from the start.
-# #' @param unfreeze (`data.table`)\cr
-# #'  A `data.table` with a column `weights` (a list column containing a `Selector`) and a column `epoch` or `batch`.
-# #' 
-# #' @family Callback
-# #' @export 
-# #' @include CallbackSet.R
-# CallbackSetUnfreeze = R6Class("CallbackSetUnfreeze",
-#   inherit = CallbackSet,
-#   lock_objects = FALSE,
-#   public = list(
-#     initialize = function(starting_weights, unfreeze) {
-#       self$starting_weights = starting_weights
-#       self$unfreeze = unfreeze
+#' @title Unfreezing Weights Callback
+#' 
+#' @name mlr_callback_set.unfreeze
+#' 
+#' @description 
+#' Unfreeze some weights after some number of steps or epochs. Select either a given module or a parameter.
+#' 
+#' @details 
+#' TODO: add
+#' 
+#' @param starting_weights (`Selector`)\cr
+#'  A `Selector` denoting the weights that are trainable from the start.
+#' @param unfreeze (`data.table`)\cr
+#'  A `data.table` with a column `weights` (a list column containing a `Selector`) and a column `epoch` or `batch`.
+#' 
+#' @family Callback
+#' @export 
+#' @include CallbackSet.R
+CallbackSetUnfreeze = R6Class("CallbackSetUnfreeze",
+  inherit = CallbackSet,
+  lock_objects = FALSE,
+  public = list(
+    initialize = function(starting_weights, unfreeze) {
+      self$starting_weights = starting_weights
+      self$unfreeze = unfreeze
 
-#       self$batch_num = 0
+      self$batch_num = 0
 
-#       # sort the unfreeze data.table
+      # sort the unfreeze data.table
       
-#       # anti-select starting weights: set grad = FALSE
-#       # selector_invert(starting_weights)
-#     },
-#     on_batch_end = function() {
-#       self$batch_num = self$batch_num + 1
-#     },
+      # anti-select starting weights: set grad = FALSE
+      # selector_invert(starting_weights)
+    },
+    on_batch_end = function() {
+      self$batch_num = self$batch_num + 1
+    },
     
-#     on_epoch_begin = function() {
-#       if (self$ctx$epoch %in% self$unfreeze$epoch) {
-#         # get the specific layer
-#         layer = self$unfreeze[epoch == self$ctx$epoch]$weights
-#         mlr3misc::map(self$ctx$network$modules[[layer]]$parameters, function(param_type) param_type$requires_grad_(FALSE))
-#       }
-#     },
+    on_epoch_begin = function() {
+      if (self$ctx$epoch %in% self$unfreeze$epoch) {
+        # get the specific layer
+        layer = self$unfreeze[epoch == self$ctx$epoch]$weights
+        mlr3misc::map(self$ctx$network$modules[[layer]]$parameters, function(param_type) param_type$requires_grad_(FALSE))
+      }
+    },
     
-#     on_batch_begin = function() {
-#       if (self$batch_num %in% self$unfreeze$batch) {
-#         layer = self$unfreeze[batch == self$batch]$weights
-#         mlr3misc::map(self$ctx$network$modules[[layer]]$parameters, function(param_type) param_type$requires_grad_(FALSE))
-#       }
-#     },
-#   ),
+    on_batch_begin = function() {
+      if (self$batch_num %in% self$unfreeze$batch) {
+        layer = self$unfreeze[batch == self$batch]$weights
+        mlr3misc::map(self$ctx$network$modules[[layer]]$parameters, function(param_type) param_type$requires_grad_(FALSE))
+      }
+    },
+  ),
   
-#   private = list(
-#   )
-# )
+  private = list(
+  )
+)
 
-# @include TorchCallback.R
-# mlr3torch_callbacks$add("unfreeze", function() {
-#   TorchCallback$new(
-#     callback_generator = CallbackSetUnfreeze,
-#     param_set = ps(
-#       starting_weights = p_uty(tags = c("train", "required")),
-#       unfreeze = p_uty(tags = c("train", "required"))
-#     ),
-#     id = "unfreeze",
-#     label = "Unfreeze",
-#     man = "mlr3torch::mlr_callback_set.unfreeze"
-#   )
-# })
+@include TorchCallback.R
+mlr3torch_callbacks$add("unfreeze", function() {
+  TorchCallback$new(
+    callback_generator = CallbackSetUnfreeze,
+    param_set = ps(
+      starting_weights = p_uty(tags = c("train", "required")),
+      unfreeze = p_uty(tags = c("train", "required"))
+    ),
+    id = "unfreeze",
+    label = "Unfreeze",
+    man = "mlr3torch::mlr_callback_set.unfreeze"
+  )
+})
---
+++ R/SelectorParam.R
@@ -0,0 +1,56 @@
+#' @title Selector Functions for Neural Network Parameters
+#' 
+#' @name SelectorParam
+#' 
+#' @description
+#' A [`SelectorParam`] function is used by the callback `CallbackSetUnfreeze` to determine a subset of parameters to freeze or unfreeze during training.
+#' 
+#' @section Details:
+#' ...
+
+make_selectorparam = function(fun, description, ...) {
+  structure(fun,
+    repr = sprintf(description, ...),
+    class = c("SelectorParam", "function")
+  )
+}
+
+#' @describeIn SelectorParam `selectorparam_all` selects all parameters
+#' @export
+selectorparam_all = function() {
+  make_selectorparam(function(param_names) {
+    param_names
+  }, "selectorparam_all()")
+}
+
+#' @describeIn SelectorParam `selectorparam_none` selects no parameters
+#' @export
+selectorparam_none = function() {
+  make_selectorparam(function(param_names) {
+    character(0)
+  }, "selectorparam_none()")
+}
+
+selector_grep = function(pattern, ignore.case = FALSE, perl = FALSE, fixed = FALSE) {
+  assert_character(pattern)
+  assert_flag(ignore.case)
+  assert_flag(perl)
+  assert_flag(fixed)
+  str_ignore_case = if (ignore.case) ", ignore.case = TRUE" else ""
+  str_perl = if (perl) ", perl = TRUE" else ""
+  str_fixed = if (fixed) ", fixed = TRUE" else ""
+  make_selectorparam(function(param_names) {
+    grep(pattern, param_names, ignore.case = ignore.case, perl = perl, fixed = fixed, value = TRUE)
+  }, "selector_grep(%s%s%s%s)", pattern, str_ignore_case, str_perl, str_fixed)
+}
+
+selector_name = function(param_names, assert_present = FALSE) {
+  assert_character(feature_names, any.missing = FALSE)
+  assert_flag(assert_present)
+  str_assert_present = if (assert_present) ", assert_present = TRUE" else ""
+  # make_selectorparam(function(task) {
+  #   if (assert_present) {
+  #     assert_subset(param_names, )
+  #   }
+  # })
+}
