729442a73b6446c3f1d41e07c0e0b11b7e4c3b9c
Carson Zhang <carsonzhang4@gmail.com>
2024-11-21 12:49:27 +0100

    still debugging

--- R/CallbackSetUnfreeze.R
+++ R/CallbackSetUnfreeze.R
@@ -1,53 +1,61 @@
 #' @title Unfreezing Weights Callback
-#' 
+#'
 #' @name mlr_callback_set.unfreeze
-#' 
-#' @description 
+#'
+#' @description
 #' Unfreeze some weights after some number of steps or epochs. Select either a given module or a parameter.
-#' 
-#' @details 
+#'
+#' @details
 #' TODO: add
-#' 
-#' @param starting_weights (`SelectorParam`)\cr
-#'  A `Selector` denoting the weights that are trainable from the start.
+#'
+#' @param starting_weights (`Select`)\cr
+#'  A `Select` denoting the weights that are trainable from the start.
 #' @param unfreeze (`data.table`)\cr
-#'  A `data.table` with a column `weights` (a list column containing a `SelectorParam`) and a column `epoch` or `batch`.
-#' 
+#'  A `data.table` with a column `weights` (a list column containing a `Select`) and a column `epoch` or `batch`.
+#'
 #' @family Callback
-#' @export 
+#' @export
 #' @include CallbackSet.R
 CallbackSetUnfreeze = R6Class("CallbackSetUnfreeze",
   inherit = CallbackSet,
   lock_objects = FALSE,
   public = list(
+    #' @description
+    #' Creates a new instance of this [R6][R6::R6Class] class.
     initialize = function(starting_weights, unfreeze) {
       self$starting_weights = starting_weights
       self$unfreeze = unfreeze
 
-      # TODO: remove
+      # TODO: remove, you can access this or compute from the information in the context
       self$batch_num = 0
 
       # sort the unfreeze data.table??
     },
+    #' @description
+    #' Sets the starting weights
     on_begin = function() {
-      weights = selectorparam_invert(starting_weights)
+      weights = select_invert(self$starting_weights)(names(mlp$network$parameters))
       walk(self$ctx$network$parameters[weights], function(param) param$requires_grad_(FALSE))
     },
+    #' @description
+    #' Increment the batch counter (old)
     on_batch_end = function() {
-      # compute from epoch and step (batch num within an epoch)
+      # TODO: compute from epoch and step (batch num within an epoch)
       self$batch_num = self$batch_num + 1
     },
+    #' @description
+    #' Unfreezes weights if the training is at the correct epoch
     on_epoch_begin = function() {
       if (self$ctx$epoch %in% self$unfreeze$epoch) {
-        # TODO: refactor to use selectors
-        weights = self$unfreeze[epoch == self$ctx$epoch]$weights
+        weights = (self$unfreeze[epoch == self$ctx$epoch]$weights)(names(self$ctx$network$parameters))
         walk(self$ctx$network$parameters[weights], function(param) param$requires_grad_(FALSE))
       }
     },
+    #' @description
+    #' Unfreezes weights if the training is at the correct batch
     on_batch_begin = function() {
       if (self$batch_num %in% self$unfreeze$batch) {
-        # TODO: refactor to use selectors
-        weights = self$unfreeze[batch == self$batch_num]$weights
+        weights = (self$unfreeze[epoch == self$ctx$epoch]$weights)(names(self$ctx$network$parameters))
         walk(self$ctx$network$parameters[weights], function(param) param$requires_grad_(FALSE))
       }
     }
--- tests/testthat/test_CallbackSetUnfreeze.R
+++ tests/testthat/test_CallbackSetUnfreeze.R
@@ -1,13 +1,13 @@
 test_that("autotest", {
   cb = t_clbk("unfreeze", 
-    starting_weights = selectorparam_all(),
+    starting_weights = select_all(),
     unfreeze = data.table()
   )
   expect_torch_callback(cb, check_man = TRUE)
 })
 
 # freeze an entire network: should train and everything is untrained at the end
-test_that("freezing an entire network works", {
+test_that("freezing some weights works", {
   n_epochs = 10
 
   task = tsk("iris")
@@ -17,156 +17,157 @@ test_that("freezing an entire network works", {
             epochs = 10, batch_size = 150, neurons = c(100, 200, 300)
   )
 
-  mlp$param_set$set_values(cb.unfreeze.starting_weights = selectorparam_none())
-  # mlp$param_set$set_values(cb.unfreeze.unfreeze = data.table(
-  #   epoch = c(2, 4),
-  #   unfreeze = list(selectorparam_grep("9"), selectorparam_))
-  # )
+  mlp$param_set$set_values(cb.unfreeze.starting_weights = select_invert(select_name(c("0.weight", "3.weight"))))
+
   mlp$param_set$set_values(cb.unfreeze.unfreeze = data.table(
-      epoch = c(1, 2),
+      epoch = 2,
+      unfreeze = select_name("0.weight")
     )
   )
-
-  list(
-
-  )
-
-  mlp$train(task)
-
-  expect_true(all(!mlr3misc::map_lgl(mlp$network$parameters, function(param) param$requires_grad)))
-})
-
-# realistic example using epochs
-test_that("weights are frozen correctly using epochs", {
-  n_epochs = 10
-
-  task = tsk("iris")
-
-  mlp = lrn("classif.mlp",
-            callbacks = t_clbk("unfreeze"),
-            epochs = 10, batch_size = 150, neurons = c(100, 200, 300)
-  )
-
-  mlp$param_set$set_values(cb.unfreeze.starting_weights = selectorparam_all())
-  # mlp$param_set$set_values(cb.unfreeze.unfreeze = data.table(
-  #   epoch = c(2, 4),
-  #   unfreeze = list(selectorparam_grep("9"), selectorparam_))
-  # )
+  
   mlp$param_set$set_values(cb.unfreeze.unfreeze = data.table())
 
   mlp$train(task)
 
-  expect_true(all(mlr3misc::map_lgl(mlp$network$parameters, function(param) param$requires_grad)))
+  # print(mlp$network$parameters[select_name("0.weight")(names(mlp$network$parameters))])
+  # print(mlp$network$parameters[select_name("3.weight")(names(mlp$network$parameters))])
 
-  # do I need a custom callback to check the internal state (context) at a given epoch?
-  # possibly, but I can also just check that at the end, the parameters look how I expect them to
-
-  # # begin LLM
-  # # Test with simple layer selection
-  # mlp$param_set$set_values(
-  #   cb.unfreeze.starting_weights = selectorparam_name(c("9.weight", "9.bias")),
-  #   cb.unfreeze.unfreeze = data.table(
-  #     weights = list(selectorparam_name("layer2")), 
-  #     epochs = 2
-  #   )
-  # )
-  
-  # # Verify initial frozen state
-  # expect_false(mlp$model$layer2$requires_grad)
-  # expect_true(mlp$model$layer1$requires_grad)
-  
-  # # Train for 3 epochs
-  # mlp$train(task)
-  # expect_true(mlp$model$layer2$requires_grad)
-  # # end LLM
+  expect_true(mlp$network$parameters[[select_name("0.weight")(names(mlp$network$parameters))]]$requires_grad)
+  expect_false(mlp$network$parameters[[select_name("3.weight")(names(mlp$network$parameters))]]$requires_grad)
+  # expect_false(any(mlr3misc::map_lgl(mlp$network$parameters, function(param) param$requires_grad)))
 })
 
-test_that("weights are frozen correctly using batches", {
-  cb = t_clbk("unfreeze")
-  n_epochs = 10
+# # realistic example using epochs
+# # TODO: write a custom callback that accesses the requires_grad of a parameter
+# # you can actually write a test for this callback as well
+# # such as: 
+# test_that("weights are frozen correctly using epochs", {
+#   n_epochs = 10
 
-  mlp = lrn("classif.mlp",
-            callbacks = t_clbk("tb"),
-            epochs = 10, batch_size = 150, neurons = c(100, 200, 300)
-  )
+#   task = tsk("iris")
 
-  # # begin LLM
-  # # Test with multiple layer unfreezing
-  # mlp$param_set$set_values(
-  #   cb.unfreeze.starting_weights = selector_none(),
-  #   cb.unfreeze.unfreeze = data.table(
-  #     weights = list(
-  #       selector_name("layer1"),
-  #       selector_name("layer2")
-  #     ),
-  #     batches = c(10, 20)
-  #   )
-  # )
+#   mlp = lrn("classif.mlp",
+#             callbacks = t_clbk("unfreeze"),
+#             epochs = 10, batch_size = 150, neurons = c(100, 200, 300)
+#   )
+
+#   mlp$param_set$set_values(cb.unfreeze.starting_weights = selectorparam_all())
+#   # mlp$param_set$set_values(cb.unfreeze.unfreeze = data.table(
+#   #   epoch = c(2, 4),
+#   #   unfreeze = list(selectorparam_grep("9"), selectorparam_))
+#   # )
+#   mlp$param_set$set_values(cb.unfreeze.unfreeze = data.table())
+
+#   mlp$train(task)
+
+#   expect_true(all(mlr3misc::map_lgl(mlp$network$parameters, function(param) param$requires_grad)))
+
+#   # # begin LLM
+#   # # Test with simple layer selection
+#   # mlp$param_set$set_values(
+#   #   cb.unfreeze.starting_weights = selectorparam_name(c("9.weight", "9.bias")),
+#   #   cb.unfreeze.unfreeze = data.table(
+#   #     weights = list(selectorparam_name("layer2")), 
+#   #     epochs = 2
+#   #   )
+#   # )
   
-  # # Verify all layers start frozen
-  # expect_false(mlp$model$layer1$requires_grad)
-  # expect_false(mlp$model$layer2$requires_grad)
+#   # # Verify initial frozen state
+#   # expect_false(mlp$model$layer2$requires_grad)
+#   # expect_true(mlp$model$layer1$requires_grad)
   
-  # mlp$train(task)
-  # expect_true(mlp$model$layer1$requires_grad)
-  # expect_true(mlp$model$layer2$requires_grad)
-  # # end LLM
-})
+#   # # Train for 3 epochs
+#   # mlp$train(task)
+#   # expect_true(mlp$model$layer2$requires_grad)
+#   # # end LLM
+# })
 
-# TODO: decide whether we want to test this (Copilot suggestion)
-test_that("invalid configurations throw errors", {
-  cb = t_clbk("unfreeze")
+# test_that("weights are frozen correctly using batches", {
+#   cb = t_clbk("unfreeze")
+#   n_epochs = 10
+
+#   mlp = lrn("classif.mlp",
+#             callbacks = t_clbk("tb"),
+#             epochs = 10, batch_size = 150, neurons = c(100, 200, 300)
+#   )
+
+#   # # begin LLM
+#   # # Test with multiple layer unfreezing
+#   # mlp$param_set$set_values(
+#   #   cb.unfreeze.starting_weights = selector_none(),
+#   #   cb.unfreeze.unfreeze = data.table(
+#   #     weights = list(
+#   #       selector_name("layer1"),
+#   #       selector_name("layer2")
+#   #     ),
+#   #     batches = c(10, 20)
+#   #   )
+#   # )
   
-  expect_error(
-    cb$param_set$set_values(
-      starting_weights = "invalid_selector",
-      unfreeze = data.table(weights = list(), epochs = numeric())
-    ),
-    "must be a Selector"
-  )
+#   # # Verify all layers start frozen
+#   # expect_false(mlp$model$layer1$requires_grad)
+#   # expect_false(mlp$model$layer2$requires_grad)
   
-  expect_error(
-    cb$param_set$set_values(
-      starting_weights = selector_none(),
-      unfreeze = data.table(weights = list(), invalid_column = numeric())
-    ),
-    "must contain either 'epochs' or 'batches'"
-  )
-})
+#   # mlp$train(task)
+#   # expect_true(mlp$model$layer1$requires_grad)
+#   # expect_true(mlp$model$layer2$requires_grad)
+#   # # end LLM
+# })
 
-# TODO: decide whether we want to test this (Copilot suggestion)
-test_that("gradual unfreezing works correctly", {
-  cb = t_clbk("unfreeze")
-  n_epochs = 5
-
-  mlp = lrn("classif.mlp",
-            callbacks = cb,
-            epochs = n_epochs, batch_size = 150, neurons = 10,
-            validate = 0.2,
-            measures_valid = msrs(c("classif.acc", "classif.ce")),
-            measures_train = msrs(c("classif.acc", "classif.ce"))
-  )
-
-  mlp$param_set$set_values(
-    cb.unfreeze.starting_weights = selector_none(),
-    cb.unfreeze.unfreeze = data.table(
-      weights = list(
-        selector_name("layer1"),
-        selector_name("layer2"),
-        selector_name("layer3")
-      ),
-      epochs = c(1, 2, 3)
-    )
-  )
-
-  # Check initial state
-  expect_false(mlp$model$layer1$requires_grad)
-  expect_false(mlp$model$layer2$requires_grad)
-  expect_false(mlp$model$layer3$requires_grad)
+# # TODO: decide whether we want to test this (Copilot suggestion)
+# test_that("invalid configurations throw errors", {
+#   cb = t_clbk("unfreeze")
   
-  # Train and check progressive unfreezing
-  mlp$train(task)
-  expect_true(mlp$model$layer1$requires_grad)
-  expect_true(mlp$model$layer2$requires_grad)
-  expect_true(mlp$model$layer3$requires_grad)
-})
+#   expect_error(
+#     cb$param_set$set_values(
+#       starting_weights = "invalid_selector",
+#       unfreeze = data.table(weights = list(), epochs = numeric())
+#     ),
+#     "must be a Selector"
+#   )
+  
+#   expect_error(
+#     cb$param_set$set_values(
+#       starting_weights = selector_none(),
+#       unfreeze = data.table(weights = list(), invalid_column = numeric())
+#     ),
+#     "must contain either 'epochs' or 'batches'"
+#   )
+# })
+
+# # TODO: decide whether we want to test this (Copilot suggestion)
+# test_that("gradual unfreezing works correctly", {
+#   cb = t_clbk("unfreeze")
+#   n_epochs = 5
+
+#   mlp = lrn("classif.mlp",
+#             callbacks = cb,
+#             epochs = n_epochs, batch_size = 150, neurons = 10,
+#             validate = 0.2,
+#             measures_valid = msrs(c("classif.acc", "classif.ce")),
+#             measures_train = msrs(c("classif.acc", "classif.ce"))
+#   )
+
+#   mlp$param_set$set_values(
+#     cb.unfreeze.starting_weights = selector_none(),
+#     cb.unfreeze.unfreeze = data.table(
+#       weights = list(
+#         selector_name("layer1"),
+#         selector_name("layer2"),
+#         selector_name("layer3")
+#       ),
+#       epochs = c(1, 2, 3)
+#     )
+#   )
+
+#   # Check initial state
+#   expect_false(mlp$model$layer1$requires_grad)
+#   expect_false(mlp$model$layer2$requires_grad)
+#   expect_false(mlp$model$layer3$requires_grad)
+  
+#   # Train and check progressive unfreezing
+#   mlp$train(task)
+#   expect_true(mlp$model$layer1$requires_grad)
+#   expect_true(mlp$model$layer2$requires_grad)
+#   expect_true(mlp$model$layer3$requires_grad)
+# })
