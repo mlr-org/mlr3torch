574da40a41a2e9caf8650bbdcb91185f1865b8f3
Carson Zhang <carsonzhang4@gmail.com>
2024-12-06 09:16:36 +0100

    started docs for callback

---
+++ R/CallbackSetLRScheduler.R
@@ -0,0 +1,8 @@
+#' @title Learning Rate Scheduling Callback
+#' 
+#' @name mlr_callback_set.lr_scheduler
+#' 
+#' @description 
+#' Changes the learning rate based on the schedule specified by a `torch` learning rate scheduler.
+#' 
+#' @param lr_scheduler ()
