5017a7f8a3b127dd646bc2aa509b189b8e54cd9a
Carson Zhang <carsonzhang4@gmail.com>
2024-12-24 08:19:04 -0600

    add comment reminder to remove the response for the dataset

--- attic/task_manual_construct-cifar.R
+++ attic/task_manual_construct-cifar.R
@@ -8,6 +8,7 @@ constructor_cifar10 = function(path) {
 
     browser()
 
+    
 }
 
 constructor_cifar10(path <- file.path(get_cache_dir(), "datasets", "cifar10"))
@@ -15,6 +16,10 @@ constructor_cifar10(path <- file.path(get_cache_dir(), "datasets", "cifar10"))
 library(here)
 
 path <- here("cache")
-fs::dir_create(path, recurse = TRUE)
 
-cifar_ds <- torchvision::cifar10_dataset(root = file.path(path), download = TRUE)
+cifar_ds <- torchvision::cifar10_dataset(root = file.path(path), download = FALSE)
+
+# first: remove the response (handled separately)
+dd = as_data_descriptor(cifar_ds, list(x = c(NA, 3, 32, 32)))
+
+
