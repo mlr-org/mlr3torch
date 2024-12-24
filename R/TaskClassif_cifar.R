362520914b546edf151f254d2ec195d7463fd937
Carson Zhang <carsonzhang4@gmail.com>
2024-12-24 07:10:31 -0600

    init

---
+++ R/TaskClassif_cifar.R
@@ -0,0 +1,20 @@
+#' @title CIFAR-10 Classification Task
+#' 
+#' @name mlr_tasks_cifar_10
+#' 
+#' @description 
+#' The CIFAR-10 subset of the 80 million tiny images dataset.
+#' The data is obtained from [`torchvision::tiny_imagenet_dataset()`].
+#' 
+#' 
+NULL
+
+# TODO: implement both CIFAR-10 and CIFAR-100 in the same file
+# the torchvision implementation and the PipeOpAdaptiveAvgPool implementations are probably helpful here
+constructor_cifar10 = function(path) {
+
+}
+
+load_task_cifar10 = function(id = "cifar10") {
+    
+}
