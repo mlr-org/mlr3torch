dce6a348323793b62ccbadd1229dc8fa1c761a97
Carson Zhang <carsonzhang4@gmail.com>
2024-12-27 07:42:15 -0600

    indent 4 -> 2

--- R/TaskClassif_cifar.R
+++ R/TaskClassif_cifar.R
@@ -10,18 +10,18 @@ NULL
 # TODO: implement both CIFAR-10 and CIFAR-100 in the same file
 # the torchvision implementation and the PipeOpAdaptiveAvgPool implementations are probably helpful here
 constructor_cifar10 = function(path) {
-    require_namespaces("torchvision")
+  require_namespaces("torchvision")
 
-    # download
-    d_train = torchvision::cifar10_dataset(root = file.path(path), train = TRUE, download = TRUE)
-    d_test = torchvision::cifar10_dataset(root = file.path(path), train = FALSE)
+  # download
+  d_train = torchvision::cifar10_dataset(root = file.path(path), train = TRUE, download = TRUE)
+  d_test = torchvision::cifar10_dataset(root = file.path(path), train = FALSE)
 
-    # look at the directory structure
-    # create a data.table with columns class, image (uri of the image in the downloaded dir)
+  # look at the directory structure
+  # create a data.table with columns class, image (uri of the image in the downloaded dir)
 }
 
 load_task_cifar10 = function(id = "cifar10") {
-    # consruct lazy tensors from the cached img uris
+  # consruct lazy tensors from the cached img uris
 
 
 }
