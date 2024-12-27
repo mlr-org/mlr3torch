d96b5f6d8f2d4c494e4881dad1497b2b13454cdc
Carson Zhang <carsonzhang4@gmail.com>
2024-12-27 08:59:36 -0600

    comments

--- R/TaskClassif_cifar.R
+++ R/TaskClassif_cifar.R
@@ -12,17 +12,79 @@ NULL
 constructor_cifar10 = function(path) {
   require_namespaces("torchvision")
 
-  # download
-  d_train = torchvision::cifar10_dataset(root = file.path(path), train = TRUE, download = TRUE)
-  d_test = torchvision::cifar10_dataset(root = file.path(path), train = FALSE)
+  torchvision::cifar10_dataset(root = path, download = TRUE)
+  
+  train_files = file.path(path, "cifar-10-batches-bin", 
+    sprintf("data_batch_%d.bin", 1:5))
+  test_file = file.path(path, "cifar-10-batches-bin", "test_batch.bin")
 
-  # look at the directory structure
-  # create a data.table with columns class, image (uri of the image in the downloaded dir)
+  train_labels = map_int(train_files, read_cifar_labels)
+
+  data.table(
+    class = factor(c(train_labels, rep(NA, times = 10000))),
+    file = c(rep(train_files, each = 10000)
+             rep(test_file, 10000)),
+    index = c(rep(1:10000, 5),
+             1:10000),
+    split = factor(rep(c("train", "test"), c(50000, 10000))),
+    ..row_id = seq_len(60000)
+  )
 }
 
 load_task_cifar10 = function(id = "cifar10") {
-  # consruct lazy tensors from the cached img uris
+  cached_constructor = function(backend) {
 
+  }
 
+  backend = DataBackendLazy$new(
+    constructor = cached_constructor,
+    rownames = seq_len(60000),
+    col_info = load_col_info("cifar10"),
+    primary_key = "..row_id"
+  )
+
+  task = TaskClassif$new(
+    backend = backend,
+    id = "cifar10",
+    target = "class",
+    label = "CIFAR-10 Classification"
+  )
+
+  backend$hash = task$man = "mlr3torch::mlr_tasks_cifar10"
+
+  task$filter(1:50000)
+
+  return(task)
 }
 
+read_cifar_labels = function(file) {
+  record_size = 1 + (32 * 32 * 3)
+  
+  # Open connection to binary file
+  con = file(file, "rb")
+  on.exit(close(con))
+  
+  # Read only the first byte of each record
+  labels = vector("integer", 10000)  # Each file has 10000 images
+  for (i in 1:10000) {
+    # TODO: debug. I think this is incorrect.
+    # readBin advances the "pointer"
+    # so we want to seek (32 * 32 * 3) bytes each time
+    # but not the first time
+    # soo... while loop?
+    # Seek to the start of each record
+    seek(con, (i - 1) * record_size)
+    # Read just the label byte
+    labels[i] = readBin(con, "integer", n = 1, size = 1)
+  }
+  
+  labels
+}
+
+read_cifar_image = function(file, idx, type = 10) {
+  record_size = 1 + (32 * 32 * 3)
+
+  
+}
+
+register_task("cifar10", load_task_cifar10)
--- data-raw/cifar.R
+++ data-raw/cifar.R
@@ -9,7 +9,6 @@ cifar_ds_test <- torchvision::cifar10_dataset(root = file.path(path), download =
 dd = as_data_descriptor(cifar_ds, list(x = c(NA, 3, 32, 32)))
 
 read_batch <- function(path, type = 10) {
-
   if (type == 10)
     n <- 10000
   else if (type == 100 && grepl("test", path))
@@ -27,7 +26,7 @@ read_batch <- function(path, type = 10) {
 
   for (i in seq_len(n)) {
 
-    labels[i] <- readBin(con, integer(), size=1, n=1, endian="big")
+  labels[i] <- readBin(con, integer(), size=1, n=1, endian="big")
 
     if (type == 100) {
       fine_labels[i] <- readBin(con, integer(), size=1, n=1, endian="big")
@@ -48,26 +47,28 @@ read_batch <- function(path, type = 10) {
     list(imgs = imgs, labels = labels)
 }
 
+# path: the full path to the batch binary file
+# i: the "global" index (1 to 60k) of the image
 read_img_from_batch = function(path, i) {
-    n = 10000
+  n = 10000
 
-    img = array(dim = c(32, 32, 3))
-    
-    con = file(path, open = "rb")
+  img = array(dim = c(32, 32, 3))
+  
+  con = file(path, open = "rb")
 
-    on.exit({close(con)}, add = TRUE)
+  on.exit({close(con)}, add = TRUE)
 
-    label = readBin(con, integer(), size = 1, n = 1, endian = "big")
+  label = readBin(con, integer(), size = 1, n = 1, endian = "big")
 
-    r = as.integer(readBin(con, raw(), size = 1, n = 1024, endian = "big"))
-    g = as.integer(readBin(con, raw(), size = 1, n = 1024, endian = "big"))
-    b = as.integer(readBin(con, raw(), size = 1, n = 1024, endian = "big"))
+  r = as.integer(readBin(con, raw(), size = 1, n = 1024, endian = "big"))
+  g = as.integer(readBin(con, raw(), size = 1, n = 1024, endian = "big"))
+  b = as.integer(readBin(con, raw(), size = 1, n = 1024, endian = "big"))
 
-    img[,,1] = matrix(r, ncol = 32, byrow = TRUE)
-    img[,,2] = matrix(g, ncol = 32, byrow = TRUE)
-    img[,,3] = matrix(b, ncol = 32, byrow = TRUE)
+  img[,,1] = matrix(r, ncol = 32, byrow = TRUE)
+  img[,,2] = matrix(g, ncol = 32, byrow = TRUE)
+  img[,,3] = matrix(b, ncol = 32, byrow = TRUE)
 
-    list(img = img, label = label)
+  list(img = img, label = label)
 }
 
 batch = read_batch(file.path(path, "cifar-10-batches-bin", "data_batch_2.bin"))
