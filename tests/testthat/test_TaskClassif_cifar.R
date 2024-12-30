1544265aba88270c9c7cb4c412724dbb5133cc37
Carson Zhang <carsonzhang4@gmail.com>
2024-12-30 16:29:07 -0600

    middle of writing test

--- tests/testthat/test_TaskClassif_cifar.R
+++ tests/testthat/test_TaskClassif_cifar.R
@@ -6,6 +6,13 @@ test_that("CIFAR-10 works", {
 
   expect_equal(task$nrow, 50000)
 
+  # idx_to_test = c(1, 2, 27, 9999,
+#   10000, 10001, 10901, 19999,
+#   20000, 20001, 29999,
+#   30000, 30001, 39999,
+#   40000, 40001, 49999,
+#   50000)
+
   task$filter(1:10)
   expect_equal(task$id, "cifar10")
   expect_equal(task$label, "CIFAR-10 Classification")
@@ -17,4 +24,53 @@ test_that("CIFAR-10 works", {
   expect_true("data.rds" %in% list.files(file.path(get_cache_dir(), "datasets", "cifar10")))
   expect_equal(task$backend$nrow, 60000)
   expect_equal(task$backend$ncol, 6)
+})
+
+test_that("CIFAR-10 data matches the torchvision implementation", {
+  withr::local_options(mlr3torch.cache = TRUE)
+  task = tsk("cifar10")
+
+  cifar10_ds_train = cifar10_dataset(root = file.path(get_cache_dir(), "datasets", "cifar10", "raw"), train = TRUE,
+    download = FALSE)
+
+  test_same_at_idx = function(idx, ds_mlr3torch, ds_torch) {
+    all.equal(as.array(ds_mlr3torch$.getitem(idx)$x), ds_torch$.getitem(idx)$x)
+  }
+  
+  train_idx = c(1, 2, 27, 9999,
+    10000, 10001, 10901, 19999,
+    20000, 20001, 29999,
+    30000, 30001, 39999,
+    40000, 40001, 49999,
+    50000)
+  task$filter(train_idx)
+  task$data()
+
+  all(map_lgl(.x = idx_to_test, .f = test_same_at_idx, ds_mlr3torch = cifar10_ds, ds_torch = tv_cifar10_ds))
+
+
+  trn_idx = 1:50000
+  int_mlr3torch_responses = as.integer(tsk_dt$class[trn_idx])
+
+  get_response = function(idx, ds) {
+    ds$.getitem(idx)$y
+  }
+  int_tv_responses = map_int(trn_idx, get_response, ds = tv_cifar10_ds)
+
+  all.equal(int_mlr3torch_responses, int_tv_responses)
+
+  train_idx = c(1, 2, 27, 9999,
+    10000, 10001, 10901, 19999,
+    20000, 20001, 29999,
+    30000, 30001, 39999,
+    40000, 40001, 49999,
+    50000)
+
+  all(map_lgl(.x = idx_to_test, .f = test_same_at_idx, ds_mlr3torch = cifar10_ds, ds_torch = tv_cifar10_ds))
+
+  test_same_at_idx(10001, cifar10_ds, tv_cifar10_ds)
+
+
+  test_idx = c(50001, 50002, 58484, 59999, 60000)
+
 })
