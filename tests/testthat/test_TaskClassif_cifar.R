e2ec6d753d99a31003ff285a91956ac4823dc6e9
Carson Zhang <carsonzhang4@gmail.com>
2024-12-30 15:58:00 -0600

    test still needs to be debugged

--- tests/testthat/test_TaskClassif_cifar.R
+++ tests/testthat/test_TaskClassif_cifar.R
@@ -2,5 +2,19 @@ skip_on_cran()
 
 test_that("CIFAR-10 works", {
   withr::local_options(mlr3torch.cache = TRUE)
-  task = tsk("tsk_cifar10")
+  task = tsk("cifar10")
+
+  expect_equal(task$nrow, 50000)
+
+  task$filter(1:10)
+  expect_equal(task$id, "cifar10")
+  expect_equal(task$label, "CIFAR-10 Classification")
+  expect_equal(task$feature_names, "image")
+  expect_equal(task$target_names, "class")
+  expect_equal(task$man, "mlr3torch::mlr_tasks_tinyimagenet")
+  task$data()
+  expect_true("cifar-10-batches-bin" %in% list.files(file.path(get_cache_dir(), "datasets", "tiny_imagenet", "raw")))
+  expect_true("data.rds" %in% list.files(file.path(get_cache_dir(), "datasets", "cifar10")))
+  expect_equal(task$backend$nrow, 60000)
+  expect_equal(task$backend$ncol, 6)
 })
