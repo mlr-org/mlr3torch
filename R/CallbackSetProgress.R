993b0def9c2e81014528ab4d005aa164a0f707a1
Sebastian Fischer <sebf.fischer@gmail.com>
2025-01-02 09:42:23 +0100

    Update R/CallbackSetProgress.R

--- R/CallbackSetProgress.R
+++ R/CallbackSetProgress.R
@@ -14,7 +14,7 @@
 #' learner = lrn("classif.mlp", epochs = 10, batch_size = 1, 
 #'   callbacks = t_clbk("progress"), validate = 0.1)
 #' learner$param_set$set_values(
-#' measures_train = msrs(c("classif.acc", "classif.ce")),
+#'   measures_train = msrs(c("classif.acc", "classif.ce")),
 #'   measures_valid = msr("classif.ce")
 #' )
 #'
