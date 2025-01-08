e1cac8748bdda9f0ab41b746272281fd501fbd9f
Carson Zhang <carsonzhang4@gmail.com>
2025-01-07 20:55:19 -0600

    tests look ok, still need to debug implementation and document

--- tests/testthat/test_CallbackSetLRScheduler.R
+++ tests/testthat/test_CallbackSetLRScheduler.R
@@ -3,8 +3,8 @@ test_that("autotest", {
   expect_torch_callback(cb)
 })
 
-test_that("decay works") {
-  cb = t_clbk("lr_scheduler_decay")
+test_that("decay works", {
+  cb = t_clbk("lr_scheduler_step")
   task = tsk("iris")
   n_epochs = 10
 
@@ -13,27 +13,48 @@ test_that("decay works") {
             epochs = n_epochs, batch_size = 150, neurons = 10,
             measures_train = msrs(c("classif.acc", "classif.ce"))
   )
-  mlp$param_set$set_values(cb.lr_scheduler.T_max = 10)
+  mlp$param_set$set_values(cb.lr_scheduler.gamma = 0.5)
+  mlp$param_set$set_values(cb.lr_scheduler.step_size = 2)
 
   mlp$train(task)
 
-  # check the lr at the end
-}
+  expect_equal(mlp$model$optimizer$param_groups[[1]]$initial_lr * (0.5)^(n_epochs / 2),
+               mlp$model$optimizer$param_groups[[1]]$lr)
+})
 
 test_that("custom LR scheduler works", {
-    # first: just see if you can train a network
-  cb = t_clbk("lr_scheduler_cosine_annealing")
+  lr_subtract <- lr_scheduler(
+    "lr_subtract",
+    initialize = function(optimizer, step_size, delta = 0.1, last_epoch = -1) {
+      self$step_size <- step_size
+      self$delta <- delta
+      super$initialize(optimizer, last_epoch)
+    },
+    get_lr = function() {
+      if ((self$last_epoch == 0) || (self$last_epoch %% self$step_size != 0)) {
+        return(sapply(self$optimizer$param_groups, function(x) x$lr))
+      }
+
+      sapply(self$optimizer$param_groups, function(x) x$lr - self$delta)
+    }
+  )
+  cb = as_lr_scheduler(lr_subtract)
+  expect_torch_callback(cb)
+
   task = tsk("iris")
   n_epochs = 10
-  set.seed(1)
+
   mlp = lrn("classif.mlp",
             callbacks = cb,
             epochs = n_epochs, batch_size = 150, neurons = 10,
             measures_train = msrs(c("classif.acc", "classif.ce"))
   )
-  mlp$param_set$set_values(cb.lr_scheduler.T_max = 10)
+  reduction_amt = 0.00001
+  mlp$param_set$set_values(cb.lr_scheduler.delta = reduction_amt)
+  mlp$param_set$set_values(cb.lr_scheduler.step_size = 2)
 
   mlp$train(task)
 
-  # check the lr at the end
+  expect_equal(mlp$model$optimizer$param_groups[[1]]$initial_lr - ((n_epoch / x) * reduction_amt),
+               mlp$model$optimizer$param_groups[[1]]$lr)
 })
