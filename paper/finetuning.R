e877385c2fe867efa824fa0c57ea3196fd9878ce
Sebastian Fischer <sebf.fischer@gmail.com>
2025-01-10 08:22:04 +0100

    ...

--- paper/finetuning.R
+++ paper/finetuning.R
@@ -1,6 +1,66 @@
 library(mlr3torch)
-library(torch)
+library(mlr3verse)
+
+
+# chunk 1
 library(torchdatasets)
+data_dir = here::here("data")
+dogs_vs_cats_dataset(data_dir, download = TRUE)
+
+# chunk 2
+ds = torch::dataset("dogs_vs_cats",
+  initialize = function(pths) {
+    self$pths = pths
+  },
+  .getitem = function(i) {
+    list(image = torchvision::base_loader(self$pths[i]))
+  },
+  .length = function() {
+    length(self$pths)
+  }
+)
+
+# chunk 3
+paths = list.files(file.path(data_dir, "dogs-vs-cats/train"), full.names = TRUE)
+dogs_vs_cats = ds(paths)
+
+# chunk 4
+lt = as_lazy_tensor(ds, list(image = NULL))
+head(lt)
+
+# chunk 5
+labels = ifelse(grepl("dog\\.\\d+\\.jpg", paths), "dog", "cat")
+table(labels)
+
+# chunk 6
+tbl = data.table(image = lt, class = label)
+task = as_task_classif(tbl, target = "class", id = "dogs_vs_cats")
+task
+
+# chunk 7
+augment = po("augment_random_vertical_flip", p = 0.5)
+
+# chunk 8
+preprocess = po("trafo_reshape", shape = c(NA, 3, 224, 224))
+
+# chunk 9
+resnet = lrn("classif.resnet18",
+  pretrained = TRUE,
+  epochs = 10,
+  validate = 1 / 3,
+  measures_valid = msrs(c("classif.logloss", "classif.acc"))
+)
+
+# chunk 10
+unfreezer = t_clbk("unfreeze",
+  starting_weights = c("fc.weights", "fc.bias"),
+  unfreeze = data.table(
+    epochs = 3, weights = select_all()
+  )
+)
+learner$set_values(
+  callbacks = list(unfreezer, t_clbk("history"))
+)
 
 
 # Define the path to the Cats vs. Dogs dataset
---
+++ paper/neural-architecture-search.R
@@ -0,0 +1,70 @@
+library(mlr3torch)
+library(mlr3verse)
+set.seed(42)
+torch_manual_seed(42)
+
+# chunk 1
+task = tsk("california_housing")
+task
+
+# chunk 2
+preprocessing = po("encode", method = "one-hot") %>>%
+  po("imputehist")
+
+# chunk 3
+ingress = po("torch_ingress_num")
+
+# chunk 4
+block = po("nn_linear", out_features = 32) %>>%
+  ppl("branch", list(relu = po("nn_relu"), tanh = po("nn_tanh"))) %>>%
+  po("nn_dropout")
+
+# chunk 5
+architecture = po("nn_block", block) %>>% po("nn_head")
+
+# chunk 6
+configuration = po("torch_loss", loss = t_loss("mse")) %>>%
+  po("torch_optimizer", optimizer = t_opt("sgd"))
+
+# chunk 7
+model = po("torch_model_regr", device = "cpu", batch_size = 128)
+
+# chunk 8
+pipeline = preprocessing %>>%
+  ingress %>>%
+   architecture %>>%
+  configuration %>>%
+  model
+
+learner = as_learner(pipeline)
+learner$id = "custom_nn"
+
+
+# chunk 9
+learner$param_set$set_values(
+  nn_block.nn_linear.out_features = to_tune(20, 200),
+  nn_block.n_blocks = to_tune(1, 5),
+  nn_block.branch.selection = to_tune(c("relu", "tanh")),
+  nn_block.nn_dropout.p = to_tune(0.3, 0.7),
+  torch_optimizer.lr = to_tune(10^-4, 10^-1, logscale = TRUE)
+)
+
+# chunk 10
+set_validate(learner, "test")
+
+# chunk 11
+learner$param_set$set_values(
+  torch_model_regr.patience = 10,
+  torch_model_regr.measures_valid = msr("regr.mse"),
+  torch_model_regr.epochs = to_tune(upper = 100, internal = TRUE)
+)
+
+# chunk 12
+ti = tune(
+  tuner = tnr("mbo"),
+  resampling = rsmp("cv", folds = 3),
+  measure = msr("internal_valid_score", minimize = TRUE),
+  learner = learner,
+  term_evals = 100,
+  task = task
+)
