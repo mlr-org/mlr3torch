62b261af0b2b9d92a9476e1f0faf5b04d92274ca
Sebastian Fischer <sebf.fischer@gmail.com>
2025-01-10 17:24:51 +0100

    improve docs

--- man-roxygen/preprocess_torchvision.R
+++ man-roxygen/preprocess_torchvision.R
@@ -9,7 +9,7 @@
 #' @description
 #' Calls [`<%= paste0("torchvision::", gsub("^(augment|trafo)", "transform", id)) %>`],
 #' see there for more information on the parameters.
-#' <%= if (pipeop$rowwise) "The preprocessing is applied row wise (no batch dimension)." else "The preprocessing is applied to the whole batch."%>
+#' <%= if (pipeop$rowwise) "The preprocessing is applied to each element of a batch individually." else "The preprocessing is applied to the whole batch."%>
 #'
 #' @section Parameters:
 #' `r mlr3misc::rd_info(po("<%= id%>")$param_set)`
