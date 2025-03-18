c341781daef5765040180db7c33b78b8f18eebbf
Carson Zhang <carsonzhang4@gmail.com>
2025-03-18 13:51:21 +0100

    idrk

--- attic/refactored-ft-transformer/R/nn_ft_transformer.R
+++ attic/refactored-ft-transformer/R/nn_ft_transformer.R
@@ -188,6 +188,22 @@ nn_cls_token = nn_module(
   }
 )
 
+reglu <- function(x) {
+  assert_true(tail(x$shape, 1) %% 2 == 0)
+  chunked = x$chunk(2, dim=-1)
+  a = chunked[[1]]
+  b = chunked[[2]]
+  return(a * nnf_relu(b))
+}
+
+geglu <- function(x) {
+  assert_true(tail(x$shape, 1) %% 2 == 0)
+  chunked = x$chunk(2, dim=-1)
+  a = chunked[[1]]
+  b = chunked[[2]]
+  return(a * nnf_gelu(b))
+}
+
 nn_reglu = nn_module(
   "nn_reglu",
   forward = function(input) {
