ca3340c38e137bc446d22b2ab4fa53ce9b6e7635
Carson Zhang <carsonzhang4@gmail.com>
2025-03-21 16:08:51 +0100

    copied popreproctorch

---
+++ R/PipeOpTorchFn.R
@@ -0,0 +1,68 @@
+#' @title Custom Function
+#' @name mlr_pipeops_nn_fn
+#' 
+#' @description
+#' Applies a user-supplied function to a tensor.
+
+#' @section Parameters:
+#' * `fn` :: `function`\cr
+#'   The function to apply. Takes a `torch` tensor as input and returns a `torch` tensor.
+#'
+#' @templateVar id nn_fn
+#' @template pipeop_torch_channels_default
+#'
+#'
+#' @export
+PipeOpTorchFn = R6Class("PipeOpTorchFn",
+  inherit = PipeOpTorch,
+  public = list(
+    #' @description Creates a new instance of this [R6][R6::R6Class] class.
+    #' @template params_pipelines
+    initialize = function(id = "nn_fn", param_vals = list()) {
+      param_set = ps(fn = p_uty(tags = "required"))
+      module_generator = nn_module("custom_fn",
+        initialize = function(fn) {
+          self$fn = fn
+        },
+        forward = function(x) {
+          return(self$fn(x))
+        }
+      )
+      super$initialize(
+        id = id,
+        param_set = param_set,
+        param_vals = param_vals,
+        module_generator = module_generator
+      )
+    }
+  ),
+  private = list(
+    .shapes_out = function(shapes_in, param_vals, task) {
+      sin = shapes_in[["input"]]
+      batch_dim = sin[1L]
+      batchdim_is_unknown = is.na(batch_dim)
+      if (batchdim_is_unknown) {
+        sin[1] = 1L
+      }
+      tensor_in = mlr3misc::invoke(torch_empty, .args = sin, device = torch_device("meta"))
+      tensor_out = tryCatch(mlr3misc::invoke(private$.fn, tensor_in, .args = param_vals),
+        error = function(e) {
+          stopf("Input shape '%s' is invalid for PipeOp with id '%s'.", shape_to_str(list(sin)), self$id)
+        }
+      )
+      sout = dim(tensor_out)
+
+      sout[1] = NA
+
+      list(sout)
+    },
+    # TODO: actually use this
+    .make_module = function(shapes_in, param_vals, task) {
+      self$param_set$values$fn
+    },
+    .fn = NULL
+  )
+)
+
+#' @include aaa.R
+register_po("nn_fn", PipeOpTorchFn)
