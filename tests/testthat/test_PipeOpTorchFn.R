d0d9f3d562f5361a6796a8816a3b0cc3ba7ce2a8
Carson Zhang <carsonzhang4@gmail.com>
2025-03-22 22:16:41 +0100

    tests, man

---
+++ tests/testthat/test_PipeOpTorchFn.R
@@ -0,0 +1,5 @@
+test_that("PipeOpTorchFn autotest", {
+  po = po("nn_fn", fn = function(x) x)
+  graph = po("torch_ingress_num") %>>% po
+  expect_pipeop_torch(graph, "nn_fn", tsk("iris"), "nn_fn")
+})
