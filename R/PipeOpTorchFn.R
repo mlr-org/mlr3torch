85aaf5bf1245b259043cf57502f765f2d75351e3
Carson Zhang <carsonzhang4@gmail.com>
2025-03-25 13:05:43 +0100

    feedback, test still not working

--- R/PipeOpTorchFn.R
+++ R/PipeOpTorchFn.R
@@ -6,12 +6,13 @@
 
 #' @section Parameters:
 #' * `fn` :: `function`\cr
-#'   The function to apply. Takes a `torch` tensor as input and returns a `torch` tensor.
+#'   The function to apply. Takes a `torch` tensor as its first argument and returns a `torch` tensor.
 #'
 #' @templateVar id nn_fn
 #' @template pipeop_torch_channels_default
 #'
-#'
+#' @example
+#' 
 #' @export
 PipeOpTorchFn = R6Class("PipeOpTorchFn",
   inherit = PipeOpTorch,
@@ -19,7 +20,10 @@ PipeOpTorchFn = R6Class("PipeOpTorchFn",
     #' @description Creates a new instance of this [R6][R6::R6Class] class.
     #' @template params_pipelines
     initialize = function(id = "nn_fn", param_vals = list()) {
-      param_set = ps(fn = p_uty(tags = c("train", "required")))
+      param_set = ps(
+        fn = p_uty(tags = c("train", "required")),
+        shapes_out = p_uty(tags = "train", custom_check = function(input) check_integer(input, null.ok = TRUE))
+      )
 
       super$initialize(
         id = id,
@@ -30,34 +34,28 @@ PipeOpTorchFn = R6Class("PipeOpTorchFn",
     }
   ),
   private = list(
-    .shapes_out = crate(function(shapes_in, param_vals, task) {
+    .shapes_out =function(shapes_in, param_vals, task) {
       sin = shapes_in[["input"]]
       batch_dim = sin[1L]
       batchdim_is_unknown = is.na(batch_dim)
       if (batchdim_is_unknown) {
         sin[1] = 1L
       }
-      tensor_in = mlr3misc::invoke(torch_empty, .args = sin, device = torch_device("meta"))
+      tensor_in = mlr3misc::invoke(torch_empty, .args = sin, device = torch_device("cpu"))
       tensor_out = tryCatch(mlr3misc::invoke(param_vals$fn, tensor_in, .args = param_vals),
         error = function(e) {
           stopf("Input shape '%s' is invalid for PipeOp with id '%s'.", shape_to_str(list(sin)), self$id)
         }
       )
       sout = dim(tensor_out)
-
-      sout[1] = NA
+      if (batchdim_is_unknown) {
+        sout[1] = NA
+      }
 
       list(sout)
-    }, .parent = topenv()),
+    },
     .make_module = function(shapes_in, param_vals, task) {
-      return(nn_module("nn_fn",
-        initialize = function(fn) {
-          self$fn = fn
-        },
-        forward = function(x) {
-          return(self$fn(x))
-        }
-      )(self$param_set$values$fn))
+      self$param_set$values$fn
     }
   )
 )
--- tests/testthat/test_PipeOpTorchFn.R
+++ tests/testthat/test_PipeOpTorchFn.R
@@ -1,6 +1,23 @@
 test_that("PipeOpTorchFn autotest", {
+  withr::local_options(mlr3torch.cache = TRUE)
   po = po("nn_fn", fn = function(x, ...) x)
-  # browser()
   graph = po("torch_ingress_num") %>>% po
   expect_pipeop_torch(graph, "nn_fn", tsk("iris"), "nn_fn")
+})
+
+test_that("PipeOpTorchFn drop dimensions", {
+  withr::local_options(mlr3torch.cache = TRUE)
+  
+  # for the tiny imagenet data, should get only the blue channel
+  po = po("nn_fn", fn = function(x) x[, -1])
+  graph = po("torch_ingress_ltnsr") %>>% po
+
+  task = tsk("tiny_imagenet")
+  task_dt = task$data()
+  
+  graph$train(task)
+  result = graph$predict(task)[[1]]
+  result_dt = result$data()
+
+  result_dt
 })
