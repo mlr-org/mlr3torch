0ac5b9292314401c7573fb34c5eaa8ecc8f5c6cb
Carson Zhang <carsonzhang4@gmail.com>
2025-05-12 08:40:36 +0200

    init -> default, shouldn't have duplicated anyway

--- attic/benchmark_ft_transformer.R
+++ attic/benchmark_ft_transformer.R
@@ -12,22 +12,64 @@ tasks = list(task_pima, task_sonar, task_spam)
 # task_mammographic_mass = as_task(otsk(id = 45557))
 # tasks = list(task_breast_cancer, task_cali_housing, task_mammographic_mass)
 
-lrn_xgboost = as_learner(ppl("robustify") %>>% lrn("classif.xgboost"))
+glrn_xgboost = as_learner(po("imputesample") %>>% lrn("classif.xgboost"))
 # TODO: factor, ordered feature types not allowed.
-# TODO: throws an error at train time when required params are missing.
-# Error in .__ParamSet__get_values(self = self, private = private, super = super,  : 
+# TODO: throws an error at train time when required params are missing, maybe it would be better if this error was thrown earlier?
+# Error in .__ParamSet__get_values(self = self, private = private, super = super,  :
 #   Missing required parameters: epochs, batch_size
 # This happened PipeOp classif.ft_transformer's $train()
-lrn_ft_transformer = as_learner(
-  ppl("robustify") %>>% 
-  lrn("classif.ft_transformer", epochs = 100, batch_size = 32)
-)
+# TODO: debug the behavior for defaults, doesn't really seem to work
 
-learners = list(lrn_xgboost, lrn_ft_transformer)
+# TODO: determine why wrapping the lrn() call in a function like this works
+# make_ft_transformer_default = function(task_type, ...) {
+#   params = list(
+#      epochs = 1L,
+#      batch_size = 32L,
+#      n_blocks = 3L,
+#      d_token = 32L,
+#      query_idx = NULL
+#   )
+#   params = insert_named(params, list(...))
+#   invoke(lrn, .key = sprintf("%s.ft_transformer", task_type), .args = params)
+# }
+# lrn_ft_transformer = make_ft_transformer_default("classif")
 
-rsmp_cv3 = rsmp("cv", folds = 3)
+lrn_ft_transformer = lrn("classif.ft_transformer",
+                         epochs = 1L,
+                         batch_size = 32L,
+                         n_blocks = 3L,
+                         d_token = 32L)
 
-design = benchmark_grid(tasks, learners, rsmp_cv3)
+# lrn_ft_transformer = lrn("classif.ft_transformer",
+#   attention_n_heads = 4,
+#   attention_dropout = 0.1,
+#   ffn_d_hidden = 100,
+#   ffn_dropout = 0.1,
+#   ffn_activation = nn_reglu,
+#   residual_dropout = 0.0,
+#   prenormalization = TRUE,
+#   is_first_layer = TRUE,
+#   attention_initialization = "kaiming",
+#   ffn_normalization = nn_layer_norm,
+#   attention_normalization = nn_layer_norm,
+#   query_idx = NULL,
+#   attention_bias = TRUE,
+#   ffn_bias_first = TRUE,
+#   ffn_bias_second = TRUE,
+
+#   # training
+#   epochs = 100L,
+#   batch_size = 32L,
+#   n_blocks = 3L,
+#   d_token = 32L
+# )
+glrn_ft_transformer = as_learner(po("imputesample") %>>% lrn_ft_transformer)
+
+learners = list(glrn_xgboost, glrn_ft_transformer)
+
+rsmp_holdout = rsmp("holdout", ratio = 0.7)
+
+design = benchmark_grid(tasks, learners, rsmp_holdout)
 
 future::plan("multisession")
 
