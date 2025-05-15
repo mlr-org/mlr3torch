0c80578c32130993e5a94a31432ac6c7c303ca47
Carson Zhang <carsonzhang4@gmail.com>
2025-05-15 16:49:06 +0200

    removed benchmark file from attic

--- R/PipeOpTorchFTTransformerBlock.R
+++ R/PipeOpTorchFTTransformerBlock.R
@@ -192,64 +192,6 @@ PipeOpTorchFTTransformerBlock = R6::R6Class("PipeOpTorchFTTransformerBlock",
 )
 mlr3pipelines::mlr_pipeops$add("nn_ft_transformer_block", PipeOpTorchFTTransformerBlock)
 
-# nn_ft_multi_head_attention = nn_module(
-#   "nn_ft_multi_head_attention",
-#   initialize = function(d_token, n_heads, dropout, bias, initialization) {
-#     if (n_heads > 1) {
-#       assert_true(d_token %% n_heads == 0)
-#     }
-#     assert_choice(initialization, c("kaiming", "xavier"))
-
-#     self$W_q = nn_linear(d_token, d_token, bias)
-#     self$W_k = nn_linear(d_token, d_token, bias)
-#     self$W_v = nn_linear(d_token, d_token, bias)
-#     self$W_out = if (n_heads > 1) nn_linear(d_token, d_token, bias) else NULL
-#     self$n_heads = n_heads
-#     self$dropout = if (dropout) nn_dropout(dropout) else NULL
-
-#     weights = c(self$W_q, self$W_k, self$W_v)
-#     for (i in seq_along(weights)) {
-#       m = weights[[i]]
-#       if (initialization == "xavier" && (i != length(weights) || !is.null(self$W_out))) {
-#         nn_init_xavier_uniform_(m$weight, gain = 1 / sqrt(2))
-#       }
-#       if (!is.null(m$bias)) nn_init_zeros_(m$bias)
-#     }
-#     if (!is.null(self$W_out)) nn_init_zeros_(self$W_out$bias)
-#   },
-#   reshape_ = function(input) {
-#     batch_size = input$shape[1]
-#     n_tokens = input$shape[2]
-#     d = input$shape[3]
-#     d_head = d %/% self$n_heads
-#     return(input$reshape(c(batch_size, n_tokens, self$n_heads, d_head))$transpose(2, 3)$reshape(c(batch_size * self$n_heads, n_tokens, d_head)))
-#   },
-#   forward = function(x_q, x_kv) {
-#     q = self$W_q(x_q)
-#     k = self$W_k(x_kv)
-#     v = self$W_v(x_kv)
-
-#     batch_size = q$shape[1]
-#     d_head_key = data.table::last(k$shape, 1) %/% self$n_heads
-#     d_head_value = data.table::last(v$shape, 1) %/% self$n_heads
-#     n_q_tokens = q$shape[2]
-
-#     q = self$reshape_(q)
-#     k = self$reshape_(k)
-#     attention_logits = torch_matmul(q, k$transpose(2, 3)) / sqrt(d_head_key)
-#     attention_probs = nnf_softmax(attention_logits, dim = -1)
-#     if (!is.null(self$dropout)) {
-#       attention_probs = self$dropout(attention_probs)
-#     }
-#     x = torch_matmul(attention_probs, self$reshape_(v))
-#     x = x$reshape(c(batch_size, self$n_heads, n_q_tokens, d_head_value))$transpose(2, 3)$reshape(c(batch_size, n_q_tokens, self$n_heads * d_head_value))
-#     if (!is.null(self$W_out)) x = self$W_out(x)
-#     return(list(x = x,
-#                 attention_logits = attention_logits,
-#                 attention_probs = attention_probs))
-#   }
-# )
-
 nn_ft_ffn = nn_module(
   "nn_ft_ffn",
   initialize = function(d_token, d_hidden, bias_first, bias_second, dropout, activation) {
