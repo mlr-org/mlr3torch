5ce4023f73588e6829da05f372ac6b3337501bc2
Sebastian Fischer <sebf.fischer@gmail.com>
2025-08-26 17:59:14 +0200

    ...

---
+++ paper/benchmark/linux-gpu-optimizer.R
@@ -0,0 +1,49 @@
+library(here)
+
+setwd(here("paper"))
+source(here("paper", "benchmark", "benchmark.R"))
+
+problem_design = expand.grid(
+  list(
+    n = N,
+    p = P,
+    epochs = EPOCHS,
+    optimizer = c("sgd", "adamw"),
+    batch_size = 32L,
+    device = "cuda",
+    n_layers = c(0L, 4L, 8L, 12L, 16L),
+    latent = c(1000L)
+  ),
+  stringsAsFactors = FALSE
+)
+
+setup(
+  here("paper", "benchmark", "registry-linux-gpu-optimizer"),
+  # This path is relative to the docker container, so no need to change it
+ "/usr/bin/python3",
+ here("paper")
+)
+
+addExperiments(
+  prob.designs = list(
+    runtime_train = problem_design
+  ),
+  algo.designs = list(
+    rtorch = data.frame(
+      jit = FALSE,
+      opt_type = c("ignite", "standard")
+    )
+  ),
+  repls = REPLS
+)
+
+tbl = unwrap(getJobTable())
+
+for (id in sample(tbl$job.id)) {
+  submitJobs(id)
+  Sys.sleep(0.1)
+}
+
+source(here("paper", "benchmark", "summarize.R"))
+result = summarize(tbl$job.id)
+saveRDS(result, here("paper", "benchmark", "result-linux-gpu-optimizer.rds"))
