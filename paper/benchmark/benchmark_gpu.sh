af4b9650df066e4190b6ba5113c8283fee429a7b
Sebastian Fischer <sebf.fischer@gmail.com>
2025-12-08 13:16:28 +0100

    update

--- paper/benchmark/benchmark_gpu.sh
+++ paper/benchmark/benchmark_gpu.sh
@@ -14,7 +14,7 @@ enroot create --force --name mlr3torch-jss sebffischer+mlr3torch-jss+gpu.sqsh
 enroot start \
   --mount /dss/dssmcmlfs01/pr74ze/pr74ze-dss-0001/ru48nas2/:/mnt/data \
   mlr3torch-jss bash -c "
-  cd /mnt/data/mlr3torch/paper
+  cd /mnt/data/paper
   Rscript -e \"
     source('benchmark/linux-gpu.R')
   \"
