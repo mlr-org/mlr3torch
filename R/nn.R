f7dab2811230d4340a8d5e1ba9289eb826232a7c
Sebastian Fischer <sebf.fischer@gmail.com>
2026-08-03 14:25:54 +0200

    fix: nn()

--- R/nn.R
+++ R/nn.R
@@ -2,7 +2,12 @@
 #' @description
 #' Retrieve a neural network layer from the
 #' [`mlr_pipeops`][mlr3pipelines::mlr_pipeops] dictionary.
+#'
+#' The id of the returned [`PipeOp`][mlr3pipelines::PipeOp] is `.key`.
+#' Because the ids within a [`Graph`][mlr3pipelines::Graph] must be unique, `.key` can be suffixed
+#' with `_<n>` to disambiguate repeated layers, e.g. `nn("linear_1")` and `nn("linear_2")`.
 #' @param .key (`character(1)`)\cr
+#'   The key of the layer in the dictionary, optionally followed by a `_<n>` suffix.
 #' @param ... (any)\cr
 #'   Additional parameters, constructor arguments or fields.
 #' @export
@@ -10,10 +15,15 @@
 #' po1 = po("nn_linear", id = "linear")
 #' # is the same as:
 #' po2 = nn("linear")
+#'
+#' # the `_<n>` suffix is part of the id, but not of the dictionary key
+#' nn("linear_1")$id
 nn = function(.key, ...) {
+  assert_string(.key)
   args = list(...)
   if (is.null(args$id)) {
     args$id = .key
   }
-  invoke(po, .obj = paste0("nn_", .key), .args = args)
+  # a `_<n>` suffix only disambiguates repeated layers within a Graph and is not part of the key
+  invoke(po, .obj = paste0("nn_", sub("_[0-9]+$", "", .key)), .args = args)
 }
--- tests/testthat/test_nn.R
+++ tests/testthat/test_nn.R
@@ -21,3 +21,25 @@ test_that("unnamed arg", {
 test_that("nn works with cov", {
   expect_equal(nn("conv3d")$id, "conv3d")
 })
+
+test_that("numeric suffix disambiguates repeated layers", {
+  x = nn("linear_1", out_features = 3)
+  expect_class(x, "PipeOpTorchLinear")
+  expect_equal(x$id, "linear_1")
+  expect_equal(x$param_set$values$out_features, 3)
+
+  expect_equal(nn("relu_2")$id, "relu_2")
+
+  # keys that end in a digit without an underscore are not affected
+  expect_equal(nn("max_pool1d")$id, "max_pool1d")
+  expect_equal(nn("relu6")$id, "relu6")
+
+  # an explicit id still wins
+  expect_equal(nn("linear_1", id = "abc")$id, "abc")
+
+  graph = po("torch_ingress_num") %>>%
+    nn("linear_1", out_features = 3) %>>%
+    nn("relu_1") %>>%
+    nn("linear_2", out_features = 4)
+  expect_equal(graph$ids(), c("torch_ingress_num", "linear_1", "relu_1", "linear_2"))
+})
